a821b1c7393cb954d6c67d6db8d42a7400722f4d
Cash DeLeon <cashd@users.noreply.github.com>
2022-10-27 18:49:23 -0500

    scaffold daisy navbar -- council-lite

--- apps/council-lite/pages/index.tsx
+++ apps/council-lite/pages/index.tsx
@@ -12,11 +12,64 @@ export default function Web() {
   return (
     <WagmiConfig client={wagmiClient}>
       <RainbowKitProvider chains={chains}>
-        <div>
-          <h1 className="font-bold">Web</h1>
-          <Button />
-          <ConnectButton />
-          <button className="btn btn-primary">Button</button>
+        <div className="navbar bg-base-100">
+          <div className="navbar-start">
+            <div className="dropdown">
+              <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />
+                </svg>
+              </label>
+              <ul
+                tabIndex={0}
+                className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+              >
+                <li>
+                  <a>proposals</a>
+                </li>
+                <li>
+                  <a>vaults</a>
+                </li>
+                <li>
+                  <a>voters</a>
+                </li>
+                <li>
+                  <a>profile</a>
+                </li>
+              </ul>
+            </div>
+            <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+          </div>
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal p-0">
+              <li>
+                <a>proposals</a>
+              </li>
+              <li>
+                <a>vaults</a>
+              </li>
+              <li>
+                <a>voters</a>
+              </li>
+              <li>
+                <a>profile</a>
+              </li>
+            </ul>
+          </div>
+          <div className="navbar-end">
+            <ConnectButton />
+          </div>
         </div>
       </RainbowKitProvider>
     </WagmiConfig>
