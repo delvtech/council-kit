e7938e88e5228c97eb71095f931807dd80e818c4
Cash DeLeon <cashd@users.noreply.github.com>
2022-10-31 15:10:49 -0500

    fixed build

--- apps/council-lite/next.config.js
+++ apps/council-lite/next.config.js
@@ -1,5 +1,8 @@
-const withTM = require("next-transpile-modules")(["ui"]);
+// const withTM = require("next-transpile-modules")(["ui"]);
+// module.exports = withTM({
+//   reactStrictMode: true,
+// });
 
-module.exports = withTM({
+module.exports = {
   reactStrictMode: true,
-});
+};
