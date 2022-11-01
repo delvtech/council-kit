bbebc3a72c566114cdf075d871b0c8b940b4c20d
Ryan Goree <goree.ryan@gmail.com>
2022-11-01 15:02:55 -0500

    Fix config key

--- apps/council-ui/.eslintrc.js
+++ apps/council-ui/.eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
     // @delve/eslint-config needs to be the last config extended because it includes prettier
     "@delve/eslint-config",
   ],
-  overrides: {
+  rules: {
     "tailwindcss/classnames-order": "off", // Disable ordering in favor of prettier plugin
   },
 };
