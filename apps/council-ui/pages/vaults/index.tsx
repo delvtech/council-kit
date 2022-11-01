c5c3d4ec6a34d5a9fb3f63f635bb2eb3d94c517b
Cash DeLeon <cashd@users.noreply.github.com>
2022-11-01 14:01:33 -0500

    moved providers and navbar to app component

--- apps/council-ui/pages/vaults/index.tsx
+++ apps/council-ui/pages/vaults/index.tsx
@@ -1,5 +1,40 @@
 import { ReactElement } from "react";
 
 export default function Vaults(): ReactElement {
-  return <>Vaults</>;
+  return (
+    <div>
+      <div className="overflow-x-auto">
+        <table className="w-full daisy-table daisy-table-zebra">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Job</th>
+              <th>Favorite Color</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th>1</th>
+              <td>Cy Ganderton</td>
+              <td>Quality Control Specialist</td>
+              <td>Blue</td>
+            </tr>
+            <tr>
+              <th>2</th>
+              <td>Hart Hagerty</td>
+              <td>Desktop Support Technician</td>
+              <td>Purple</td>
+            </tr>
+            <tr>
+              <th>3</th>
+              <td>Brice Swyre</td>
+              <td>Tax Accountant</td>
+              <td>Red</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
