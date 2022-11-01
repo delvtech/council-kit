ae47c0048c47e7a8d193a18e1eb715a96f2ff96b
Cash DeLeon <cashd@users.noreply.github.com>
2022-11-01 15:27:56 -0500

    added details button

--- apps/council-ui/pages/vaults/index.tsx
+++ apps/council-ui/pages/vaults/index.tsx
@@ -3,12 +3,12 @@ import { ReactElement } from "react";
 export default function Vaults(): ReactElement {
   return (
     <div className="max-w-5xl m-auto">
-      <div className="flex flex-col items-center pl-16 pr-16 mt-16 space-y-8">
+      <div className="flex flex-col items-center pl-16 pr-16 mt-16 space-y-6">
         <div className="w-full text-2xl text-left text-white">Vaults</div>
-        <div className="w-full text-lg text-left text-white">
+        <div className="w-full text-left text-white text-md">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris{" "}
+          minim veniam, quis nostrud exercitation ullamco laboris
         </div>
         <table className="w-full daisy-table daisy-table-zebra min-w-fit">
           <thead>
@@ -17,6 +17,7 @@ export default function Vaults(): ReactElement {
               <th>Name</th>
               <th>total voting power</th>
               <th>your voting power</th>
+              <th></th>
             </tr>
           </thead>
           <tbody>
@@ -25,36 +26,66 @@ export default function Vaults(): ReactElement {
               <td>Locking Vault</td>
               <td>1,000,000</td>
               <td>50,000</td>
+              <th>
+                <button className="daisy-btn daisy-btn-ghost daisy-btn-sm">
+                  ▹
+                </button>
+              </th>
             </tr>
             <tr>
               <th className="underline">0x000...000</th>
               <td>Locking Vault</td>
               <td>1,000,000</td>
               <td>50,000</td>
+              <th>
+                <button className="daisy-btn daisy-btn-ghost daisy-btn-sm">
+                  ▹
+                </button>
+              </th>
             </tr>
             <tr>
               <th className="underline">0x000...000</th>
               <td>Locking Vault</td>
               <td>1,000,000</td>
               <td>50,000</td>
+              <th>
+                <button className="daisy-btn daisy-btn-ghost daisy-btn-sm">
+                  ▹
+                </button>
+              </th>
             </tr>
             <tr>
               <th className="underline">0x000...000</th>
               <td>Locking Vault</td>
               <td>1,000,000</td>
               <td>50,000</td>
+              <th>
+                <button className="daisy-btn daisy-btn-ghost daisy-btn-sm">
+                  ▹
+                </button>
+              </th>
             </tr>
             <tr>
               <th className="underline">0x000...000</th>
               <td>Locking Vault</td>
               <td>1,000,000</td>
               <td>50,000</td>
+              <th>
+                <button className="daisy-btn daisy-btn-ghost daisy-btn-sm">
+                  ▹
+                </button>
+              </th>
             </tr>
             <tr>
               <th className="underline">0x000...000</th>
               <td>Locking Vault</td>
               <td>1,000,000</td>
               <td>50,000</td>
+              <th>
+                <button className="daisy-btn daisy-btn-ghost daisy-btn-sm">
+                  ▹
+                </button>
+              </th>
             </tr>
           </tbody>
         </table>
