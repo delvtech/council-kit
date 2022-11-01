58f90f45574b8880de7663840a9a307687e1603c
Cash DeLeon <cashd@users.noreply.github.com>
2022-11-01 14:25:54 -0500

    vault list page scaffold

--- apps/council-ui/pages/vaults/index.tsx
+++ apps/council-ui/pages/vaults/index.tsx
@@ -2,35 +2,59 @@ import { ReactElement } from "react";
 
 export default function Vaults(): ReactElement {
   return (
-    <div>
-      <div className="overflow-x-auto">
-        <table className="w-full daisy-table daisy-table-zebra">
+    <div className="max-w-5xl m-auto">
+      <div className="flex flex-col items-center pl-16 pr-16 mt-16 space-y-8">
+        <div className="w-full text-2xl text-left text-white">Vaults</div>
+        <div className="w-full text-lg text-left text-white">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris{" "}
+        </div>
+        <table className="w-full daisy-table daisy-table-zebra min-w-fit">
           <thead>
             <tr>
-              <th></th>
+              <th>Address</th>
               <th>Name</th>
-              <th>Job</th>
-              <th>Favorite Color</th>
+              <th>total voting power</th>
+              <th>your voting power</th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <th>1</th>
-              <td>Cy Ganderton</td>
-              <td>Quality Control Specialist</td>
-              <td>Blue</td>
+              <th className="underline">0x000...000</th>
+              <td>Locking Vault</td>
+              <td>1,000,000</td>
+              <td>50,000</td>
             </tr>
             <tr>
-              <th>2</th>
-              <td>Hart Hagerty</td>
-              <td>Desktop Support Technician</td>
-              <td>Purple</td>
+              <th className="underline">0x000...000</th>
+              <td>Locking Vault</td>
+              <td>1,000,000</td>
+              <td>50,000</td>
             </tr>
             <tr>
-              <th>3</th>
-              <td>Brice Swyre</td>
-              <td>Tax Accountant</td>
-              <td>Red</td>
+              <th className="underline">0x000...000</th>
+              <td>Locking Vault</td>
+              <td>1,000,000</td>
+              <td>50,000</td>
+            </tr>
+            <tr>
+              <th className="underline">0x000...000</th>
+              <td>Locking Vault</td>
+              <td>1,000,000</td>
+              <td>50,000</td>
+            </tr>
+            <tr>
+              <th className="underline">0x000...000</th>
+              <td>Locking Vault</td>
+              <td>1,000,000</td>
+              <td>50,000</td>
+            </tr>
+            <tr>
+              <th className="underline">0x000...000</th>
+              <td>Locking Vault</td>
+              <td>1,000,000</td>
+              <td>50,000</td>
             </tr>
           </tbody>
         </table>
