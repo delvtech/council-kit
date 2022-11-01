54ca2cd4861bfbc7cbcce454e699b101d2b9cb69
Ryan Goree <goree.ryan@gmail.com>
2022-11-01 14:54:29 -0500

    Update eslint

--- apps/council-ui/.eslintrc.js
+++ apps/council-ui/.eslintrc.js
@@ -1,4 +1,13 @@
 module.exports = {
   root: true,
-  extends: ["@delve/eslint-config"],
+  extends: [
+    "next",
+    "plugin:react-hooks/recommended",
+    "plugin:tailwindcss/recommended",
+    // @delve/eslint-config needs to be the last config extended because it includes prettier
+    "@delve/eslint-config",
+  ],
+  overrides: {
+    "tailwindcss/classnames-order": "off", // Disable ordering in favor of prettier plugin
+  },
 };
