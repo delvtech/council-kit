5f1f3178d2c307490660a60cdb4be4ef9c390836
Ryan Goree <goree.ryan@gmail.com>
2022-11-03 12:52:59 -0500

    Move .DS_Store gitignore and fix menu svg:

--- apps/council-ui/pages/_app.tsx
+++ apps/council-ui/pages/_app.tsx
@@ -23,14 +23,20 @@ function MyApp({ Component, pageProps }: AppProps): ReactElement {
                 tabIndex={0}
                 className="daisy-btn-ghost daisy-btn lg:hidden"
               >
-                <Link href="/">
-                  <Image
-                    alt="Council"
-                    src="/council-logo.svg"
-                    width={200}
-                    height={52}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
                   />
-                </Link>
+                </svg>
               </label>
               <ul
                 tabIndex={0}
