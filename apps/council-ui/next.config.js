257711c1e4813fabba635f4d58d43333dbd6afd0
Ryan Goree <goree.ryan@gmail.com>
2022-11-03 15:37:19 -0500

    Add comment

--- apps/council-ui/next.config.js
+++ apps/council-ui/next.config.js
@@ -6,8 +6,10 @@ module.exports = {
   // js chunks and css files should have "./" appended to them so that GitHub
   // pages can resolve them.
   assetPrefix: "./",
+  // This makes it possible to call `next export` when using next/image with
+  // imported assets
   images: {
-    loader: 'akamai',
-    path: './'
-  }
+    loader: "akamai",
+    path: "./",
+  },
 };
