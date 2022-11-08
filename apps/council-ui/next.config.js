c876de298ab49bbe3e6ff2955d0545b2c5f69308
Cash DeLeon <cashd@users.noreply.github.com>
2022-11-08 12:58:28 -0600

    Fix hot reloading

--- apps/council-ui/next.config.js
+++ apps/council-ui/next.config.js
@@ -1,15 +1,34 @@
+/* eslint-disable @typescript-eslint/no-var-requires*/
+const { PHASE_DEVELOPMENT_SERVER } = require("next/dist/shared/lib/constants");
+
 /**
  * @type {import('next').NextConfig}
  */
-module.exports = {
-  reactStrictMode: true,
-  // js chunks and css files should have "./" appended to them so that GitHub
-  // pages can resolve them.
-  assetPrefix: "./",
-  // This makes it possible to call `next export` when using next/image with
-  // imported assets
-  images: {
-    loader: "akamai",
-    path: "./",
-  },
+module.exports = (phase) => {
+  // Development Config
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      reactStrictMode: true,
+      // This makes it possible to call `next export` when using next/image with
+      // imported assets
+      images: {
+        loader: "akamai",
+        path: "./",
+      },
+    };
+  } else {
+    // Production Config
+    return {
+      reactStrictMode: true,
+      // js chunks and css files should have "./" appended to them so that GitHub
+      // pages can resolve them.
+      assetPrefix: "./",
+      // This makes it possible to call `next export` when using next/image with
+      // imported assets
+      images: {
+        loader: "akamai",
+        path: "./",
+      },
+    };
+  }
 };
