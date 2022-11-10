129cb02bdb612d9841094353f5a5a6e974a4e350
Cash DeLeon <cashd@users.noreply.github.com>
2022-11-09 18:25:18 -0600

    vaults details refactor

--- apps/council-ui/components/base/Container.tsx
+++ apps/council-ui/components/base/Container.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren, ReactElement } from "react";
 
 export function Container({ children }: PropsWithChildren): ReactElement {
   return (
-    <div className="m-auto mt-16 flex max-w-5xl flex-col items-center gap-y-10 px-4">
+    <div className="m-auto mt-16 flex max-w-5xl flex-col gap-y-10 px-4">
       {children}
     </div>
   );
