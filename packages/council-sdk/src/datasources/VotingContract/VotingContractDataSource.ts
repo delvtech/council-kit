68e8c6b6dfed267b5c29879382f94d1c4ea4a2af
Ryan Goree <goree.ryan@gmail.com>
2022-11-09 20:00:34 -0600

    [SDK] Add voting contract datasource and connect models (#42)
    * Add voting contract data source
    
    * Connect models to voting contract data source
    
    * Add getExecutedProposalIds method
    
    * Use ProposalExecuted events for Proposal.isExecuted

---
+++ packages/council-sdk/src/datasources/VotingContract/VotingContractDataSource.ts
@@ -0,0 +1,45 @@
+export interface ProposalDataPreview {
+  id: number;
+  createdBlock: number;
+  unlockBlock: number;
+  expirationBlock: number;
+}
+
+export interface ProposalData extends ProposalDataPreview {
+  hash: string;
+  requiredQuorum: string;
+  lastCallBlock: number;
+}
+
+export type Ballot = "yes" | "no" | "maybe";
+
+export interface VoteData {
+  address: string;
+  proposalId: number;
+  power: string;
+  ballot: Ballot;
+}
+
+export type VoteResults = Record<Ballot, string>;
+
+export interface VotingContractDataSource {
+  address: string;
+  getProposalCount: () => Promise<number>;
+  getProposal: (id: number) => Promise<ProposalData | null>;
+  getProposals: (
+    fromBlock?: number,
+    toBlock?: number,
+  ) => Promise<ProposalDataPreview[]>;
+  getExecutedProposalIds: (
+    fromBlock?: number,
+    toBlock?: number,
+  ) => Promise<number[]>;
+  getVote: (address: string, proposalId: number) => Promise<VoteData>;
+  getVotes: (
+    address?: string,
+    proposalId?: number,
+    fromBlock?: number,
+    toBlock?: number,
+  ) => Promise<VoteData[]>;
+  getResults: (proposalId: number) => Promise<VoteResults>;
+}
