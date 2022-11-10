2cd00bf1b73ea5bc8d45160483f63a78da547d3f
Cash DeLeon <cashd@users.noreply.github.com>
2022-11-09 18:09:38 -0600

    prettier run

--- apps/council-ui/components/base/Container.tsx
+++ apps/council-ui/components/base/Container.tsx
@@ -1,6 +1,6 @@
-import { ReactNode, PropsWithChildren } from "react";
+import { PropsWithChildren, ReactElement } from "react";
 
-export function Container({ children }: PropsWithChildren): ReactNode {
+export function Container({ children }: PropsWithChildren): ReactElement {
   return (
     <div className="m-auto mt-16 flex max-w-5xl flex-col items-center gap-y-10 px-4">
       {children}
