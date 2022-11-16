02352284a0240234c2848f04a4672f242bccbebe
Cash Baller <cashd@users.noreply.github.com>
2022-11-16 16:28:41 -0600

    FIX GH PAGES PATHS (#59)

--- apps/council-ui/next.config.js
+++ apps/council-ui/next.config.js
@@ -22,12 +22,12 @@ module.exports = (phase) => {
       reactStrictMode: true,
       // js chunks and css files should have "./" appended to them so that GitHub
       // pages can resolve them.
-      assetPrefix: "./",
+      basePath: "/council-monorepo",
       // This makes it possible to call `next export` when using next/image with
       // imported assets
       images: {
         loader: "akamai",
-        path: "./",
+        path: "council-monorepo/",
       },
     };
   }
