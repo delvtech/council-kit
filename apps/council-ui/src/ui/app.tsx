31d8132e5ed82ba99b9668d5e2ef6d81eef68149
Danny Delott <dannydelott@gmail.com>
2022-11-18 12:09:24 -0800

    Move navigation to own component

--- apps/council-ui/src/ui/app.tsx
+++ apps/council-ui/src/ui/app.tsx
@@ -1,143 +1,24 @@
-import { ConnectButton, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { QueryClientProvider } from "@tanstack/react-query";
-import classNames from "classnames";
 import type { AppProps } from "next/app";
-import Link from "next/link";
-import { useRouter } from "next/router";
 import { ReactElement } from "react";
 import { reactQueryClient } from "src/clients/reactQuery";
 import { wagmiClient } from "src/clients/wagmi";
 import { councilConfigs } from "src/config/council.config";
 import { chains } from "src/provider";
-import { makeVoterURL, Routes } from "src/routes";
 import { CouncilClientProvider } from "src/ui/council/CouncilProvider";
-import { useAccount, WagmiConfig } from "wagmi";
+import { Navigation } from "src/ui/navigation/Navigation";
+import { WagmiConfig } from "wagmi";
 
 console.log(councilConfigs);
 
 function App({ Component, pageProps }: AppProps): ReactElement {
-  const { address } = useAccount();
-
-  const { pathname } = useRouter();
-
   return (
     <WagmiConfig client={wagmiClient}>
       <RainbowKitProvider chains={chains}>
         <QueryClientProvider client={reactQueryClient}>
           <CouncilClientProvider>
-            <div className="daisy-navbar bg-base-100">
-              <div className="daisy-navbar-start">
-                <div className="daisy-dropdown">
-                  <label
-                    tabIndex={0}
-                    className="daisy-btn-ghost daisy-btn lg:hidden"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-5 w-5"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M4 6h16M4 12h8m-8 6h16"
-                      />
-                    </svg>
-                  </label>
-                  <ul
-                    tabIndex={0}
-                    className="daisy-dropdown-content daisy-menu rounded-box daisy-menu-compact mt-3 w-52 bg-base-100 p-2 shadow"
-                  >
-                    <li>
-                      <Link
-                        className={classNames({
-                          "daisy-active": pathname.startsWith(Routes.PROPOSALS),
-                        })}
-                        href={Routes.PROPOSALS}
-                      >
-                        proposals
-                      </Link>
-                    </li>
-                    <li>
-                      <Link
-                        className={classNames({
-                          "daisy-active": pathname.startsWith(Routes.VAULTS),
-                        })}
-                        href={Routes.VAULTS}
-                      >
-                        vaults
-                      </Link>
-                    </li>
-                    <li>
-                      <Link
-                        className={classNames({
-                          "daisy-active": pathname.startsWith(Routes.VOTERS),
-                        })}
-                        href={Routes.VOTERS}
-                      >
-                        voters
-                      </Link>
-                    </li>
-                    {address && (
-                      <li>
-                        <Link href={makeVoterURL(address)}>profile</Link>
-                      </li>
-                    )}
-                  </ul>
-                </div>
-                <div className="ml-2 whitespace-nowrap text-xl text-accent-content">
-                  council-ui 🫡
-                </div>
-                {/* <Link href="/">
-              <Image alt="Council" src={CouncilLogo} width={200} height={52} />
-            </Link> */}
-              </div>
-              <div className="daisy-navbar-center hidden lg:flex">
-                <ul className="daisy-menu daisy-menu-horizontal p-0">
-                  <li>
-                    <Link
-                      className={classNames({
-                        "daisy-active": pathname.startsWith(Routes.PROPOSALS),
-                      })}
-                      href={Routes.PROPOSALS}
-                    >
-                      proposals
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      className={classNames({
-                        "daisy-active": pathname.startsWith(Routes.VAULTS),
-                      })}
-                      href={Routes.VAULTS}
-                    >
-                      vaults
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      className={classNames({
-                        "daisy-active": pathname.startsWith(Routes.VOTERS),
-                      })}
-                      href={Routes.VOTERS}
-                    >
-                      voters
-                    </Link>
-                  </li>
-                  {address && (
-                    <li>
-                      <Link href={makeVoterURL(address)}>profile</Link>
-                    </li>
-                  )}
-                </ul>
-              </div>
-              <div className="daisy-navbar-end">
-                <ConnectButton />
-              </div>
-            </div>
+            <Navigation />
             <main>
               <Component {...pageProps} />
             </main>
