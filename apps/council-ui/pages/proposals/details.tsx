e0f0d3b52a8c636eeb5b60e97e5e5f3f44591fa4
Cash DeLeon <cashd@users.noreply.github.com>
2022-11-18 16:58:22 -0600

    Componentize and integrate read-only methods to the proposal details page

--- apps/council-ui/pages/proposals/details.tsx
+++ apps/council-ui/pages/proposals/details.tsx
@@ -1,300 +1,204 @@
-import { getBlockDate, Proposal, VotingContract } from "@council/sdk";
+import { getBlockDate, Vote } from "@council/sdk";
 import { useQuery } from "@tanstack/react-query";
 import { useRouter } from "next/router";
 import { ReactElement } from "react";
 import { useCouncil } from "src/ui/council/useCouncil";
 import QuorumBar from "src/ui/QuorumBar";
-import { useProvider } from "wagmi";
+import { formatBalance } from "src/ui/utils/formatBalance";
+import ProposalVoting from "src/ui/voting/ProposalVoting";
+import { useAccount } from "wagmi";
 
-function useProposal(
-  proposalId: number,
-  votingContract: VotingContract | string,
-) {
-  const { context } = useCouncil();
-  return useQuery(
-    [votingContract, proposalId],
-    async () => {
-      return new Proposal(proposalId, votingContract, context);
-    },
-    {
-      refetchOnWindowFocus: false,
-    },
+// TODO @cashd: stats bar returns unix timestamp of 0
+export default function ProposalPage(): ReactElement {
+  const {
+    query: { id },
+  } = useRouter();
+  const proposalId = +(id as string);
+
+  const { address } = useAccount();
+  const { data } = useProposalDetailsPageData(proposalId);
+
+  return (
+    <div className="m-auto mt-16 flex max-w-5xl flex-col items-start gap-y-10 px-4">
+      <div className="flex w-full flex-wrap items-center gap-4">
+        <h1 className="mb-4 whitespace-nowrap text-5xl text-accent-content underline">
+          Proposal {proposalId ?? "🤷"}
+        </h1>
+
+        {data?.currentQuorum && data?.requiredQuorum && (
+          <div className="sm:ml-auto">
+            <QuorumBar
+              current={data?.currentQuorum}
+              required={data?.requiredQuorum}
+            />
+          </div>
+        )}
+      </div>
+
+      <ProposalStatsBar
+        createdAt={data?.createdAt}
+        endsAt={data?.endsAt}
+        unlockedAt={data?.unlockedAt}
+        lastCallAt={data?.lastCallAt}
+      />
+
+      <div className="flex w-full flex-wrap gap-10 sm:gap-y-0">
+        <div className="flex min-w-[280px] grow flex-col gap-y-4 sm:basis-[50%]">
+          <h1 className="text-2xl text-accent-content">Voting Activity</h1>
+          <ProposalVotingActivity votes={data?.votes} />
+        </div>
+
+        <div className="grow basis-[300px] md:grow-0">
+          <ProposalVoting account={address} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function useProposalDetailsPageData(proposalId?: number) {
+  const { context, coreVoting } = useCouncil();
+  const provider = context.provider;
+
+  return useQuery([], async () => {
+    const proposal = coreVoting.getProposal(proposalId!);
+
+    const currentQuorum = await proposal!.getCurrentQuorum();
+    const requiredQuorum = await proposal!.getRequiredQuorum();
+
+    const createdAt = await getBlockDate(
+      await +proposal!.getCreatedBlock(),
+      provider,
+    );
+
+    const endsAt = await getBlockDate(
+      await +proposal!.getExpirationBlock(),
+      provider,
+    );
+
+    const unlockedAt = await getBlockDate(
+      await +proposal!.getUnlockBlock(),
+      provider,
+    );
+
+    const lastCallAt = await getBlockDate(
+      await +proposal!.getLastCallBlock(),
+      provider,
+    );
+
+    const votes = await proposal?.getVotes();
+
+    return {
+      proposal,
+      currentQuorum,
+      requiredQuorum,
+      createdAt,
+      endsAt,
+      unlockedAt,
+      lastCallAt,
+      votes,
+    };
+  });
+}
+
+interface ProposalStatsBarProps {
+  createdAt?: Date | null;
+  endsAt?: Date | null;
+  unlockedAt?: Date | null;
+  lastCallAt?: Date | null;
+}
+
+function ProposalStatsBar({
+  createdAt,
+  endsAt,
+  unlockedAt,
+  lastCallAt,
+}: ProposalStatsBarProps): ReactElement {
+  return (
+    <div className="flex flex-wrap gap-4">
+      <div className="daisy-stats">
+        <div className="daisy-stat bg-base-300">
+          <div className="daisy-stat-title">Voting Contract</div>
+          <div className="daisy-stat-value text-sm">Core Voting</div>
+        </div>
+      </div>
+
+      <div className="daisy-stats">
+        <div className="daisy-stat bg-base-300">
+          <div className="daisy-stat-title">Created</div>
+          <div className="daisy-stat-value text-sm">
+            {createdAt?.toLocaleDateString()}
+          </div>
+        </div>
+      </div>
+
+      <div className="daisy-stats">
+        <div className="daisy-stat bg-base-300">
+          <div className="daisy-stat-title">Voting Ends</div>
+          <div className="daisy-stat-value text-sm">
+            {endsAt?.toLocaleDateString()}
+          </div>
+        </div>
+      </div>
+
+      <div className="daisy-stats">
+        <div className="daisy-stat bg-base-300">
+          <div className="daisy-stat-title">Unlocked</div>
+          <div className="daisy-stat-value text-sm">
+            {unlockedAt?.toLocaleDateString()}
+          </div>
+        </div>
+      </div>
+
+      <div className="daisy-stats">
+        <div className="daisy-stat bg-base-300">
+          <div className="daisy-stat-title">Last Call</div>
+          <div className="daisy-stat-value text-sm">
+            {lastCallAt?.toLocaleDateString()}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+interface ProposalVotingActivityProps {
+  votes?: Vote[];
+}
+
+function ProposalVotingActivity({
+  votes,
+}: ProposalVotingActivityProps): ReactElement {
+  return (
+    <>
+      <div className="grid grid-cols-3">
+        <h2>Voter</h2>
+        <h2>Voting Power</h2>
+        <h2>Ballot</h2>
+      </div>
+      <input
+        type="text"
+        placeholder="Search"
+        className="daisy-input-bordered daisy-input bg-base-200"
+      />
+      {votes?.map((vote) => {
+        const voter = vote.voter;
+        return (
+          <div className="grid grid-cols-3" key={voter.address}>
+            <h2 className="underline">
+              {/* TODO @cashd: find a way to get ENS or address */}
+              {voter.address.slice(0, 5)}...{voter.address.slice(-4)}
+            </h2>
+            <h2>{formatBalance(vote.power)}</h2>
+            <h2 className="text-green-400">{vote.ballot.toUpperCase()}</h2>
+          </div>
+        );
+      })}
+    </>
   );
 }
 
 interface ProposalQuorum {
   currentQuorum?: string | null;
   requiredQuorum?: string | null;
-}
-
-function useProposalQuorum(proposal?: Proposal) {
-  return useQuery<ProposalQuorum>(
-    [proposal],
-    async () => {
-      const currentQuorum = await proposal!.getCurrentQuorum();
-      const requiredQuorum = await proposal!.getRequiredQuorum();
-      return {
-        currentQuorum,
-        requiredQuorum,
-      };
-    },
-    {
-      enabled: !!proposal,
-    },
-  );
-}
-
-function useProposalStats(proposal?: Proposal) {
-  const provider = useProvider();
-  return useQuery(
-    [proposal, !!proposal],
-    async () => {
-      const createdAt = await getBlockDate(
-        await +proposal!.getCreatedBlock(),
-        provider,
-      );
-
-      const endsAt = await getBlockDate(
-        await +proposal!.getExpirationBlock(),
-        provider,
-      );
-
-      const unlockedAt = await getBlockDate(
-        await +proposal!.getUnlockBlock(),
-        provider,
-      );
-
-      const lastCallAt = await getBlockDate(
-        await +proposal!.getLastCallBlock(),
-        provider,
-      );
-
-      return {
-        createdAt,
-        endsAt,
-        unlockedAt,
-        lastCallAt,
-      };
-    },
-    {
-      enabled: !!proposal,
-      refetchOnWindowFocus: false,
-      refetchOnMount: false,
-      refetchOnReconnect: false,
-      retry: false,
-    },
-  );
-}
-
-export default function ProposalPage(): ReactElement {
-  const {
-    query: { coreVotingAddressParam, idParam },
-  } = useRouter();
-
-  const proposalId = +(idParam as string);
-  const coreVotingAddress = coreVotingAddressParam as string;
-
-  const { data: proposal } = useProposal(proposalId, coreVotingAddress);
-
-  //console.log(proposal, !!proposal);
-  const { data: proposalQuorum } = useProposalQuorum(proposal);
-  //console.log(proposalQuorum);
-
-  const { data: stats } = useProposalStats(proposal);
-
-  return (
-    <div className="m-auto mt-16 flex max-w-5xl flex-col items-start gap-y-10 px-4">
-      {/* Page Header */}
-      <div className="flex w-full flex-wrap items-center gap-4">
-        <h1 className="mb-4 whitespace-nowrap text-5xl text-accent-content underline">
-          Proposal {proposalId}
-        </h1>
-
-        {proposalQuorum?.currentQuorum && proposalQuorum?.requiredQuorum && (
-          <div className="sm:ml-auto">
-            <QuorumBar
-              current={proposalQuorum.currentQuorum}
-              required={proposalQuorum.requiredQuorum}
-            />
-          </div>
-        )}
-      </div>
-
-      {/* Statistics Row */}
-      <div className="flex flex-wrap gap-4">
-        <div className="daisy-stats">
-          <div className="daisy-stat bg-base-300">
-            <div className="daisy-stat-title">Voting Contract</div>
-            <div className="daisy-stat-value text-sm">Core Voting</div>
-          </div>
-        </div>
-
-        <div className="daisy-stats">
-          <div className="daisy-stat bg-base-300">
-            <div className="daisy-stat-title">Created</div>
-            <div className="daisy-stat-value text-sm">
-              {stats?.createdAt?.toLocaleDateString()}
-            </div>
-          </div>
-        </div>
-
-        <div className="daisy-stats">
-          <div className="daisy-stat bg-base-300">
-            <div className="daisy-stat-title">Voting Ends</div>
-            <div className="daisy-stat-value text-sm">
-              {stats?.endsAt?.toLocaleDateString()}
-            </div>
-          </div>
-        </div>
-
-        <div className="daisy-stats">
-          <div className="daisy-stat bg-base-300">
-            <div className="daisy-stat-title">Unlocked</div>
-            <div className="daisy-stat-value text-sm">
-              {stats?.unlockedAt?.toLocaleDateString()}
-            </div>
-          </div>
-        </div>
-
-        <div className="daisy-stats">
-          <div className="daisy-stat bg-base-300">
-            <div className="daisy-stat-title">Last Call</div>
-            <div className="daisy-stat-value text-sm">
-              {stats?.lastCallAt?.toLocaleDateString()}
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="flex w-full flex-wrap gap-10 sm:gap-y-0">
-        <div className="flex min-w-[280px] grow flex-col gap-y-4 sm:basis-[50%]">
-          <h1 className="text-2xl text-accent-content">Voting Activity</h1>
-          <div className="grid grid-cols-3">
-            <h2>Voter</h2>
-            <h2>Voting Power</h2>
-            <h2>Ballot</h2>
-          </div>
-          <input
-            type="text"
-            placeholder="Search"
-            className="daisy-input-bordered daisy-input-accent daisy-input bg-base-300"
-          />
-
-          {/* Data Rows */}
-          <div className="grid grid-cols-3">
-            <h2 className="underline">xashd.eth</h2>
-            <h2>80,000</h2>
-            <h2 className="text-green-400">YES</h2>
-          </div>
-
-          <div className="grid grid-cols-3">
-            <h2 className="underline">xashd.eth</h2>
-            <h2>80,000</h2>
-            <h2 className="text-green-400">YES</h2>
-          </div>
-
-          <div className="grid grid-cols-3">
-            <h2 className="underline">xashd.eth</h2>
-            <h2>80,000</h2>
-            <h2 className="text-green-400">YES</h2>
-          </div>
-
-          <div className="grid grid-cols-3">
-            <h2 className="underline">xashd.eth</h2>
-            <h2>80,000</h2>
-            <h2 className="text-green-400">YES</h2>
-          </div>
-
-          <div className="grid grid-cols-3">
-            <h2 className="underline">xashd.eth</h2>
-            <h2>80,000</h2>
-            <h2 className="text-green-400">YES</h2>
-          </div>
-        </div>
-
-        <div className="grow basis-[300px] md:grow-0">
-          <div className="flex flex-col gap-y-4">
-            <h2 className="text-2xl text-accent-content">Your Vote</h2>
-            <div className="flex">
-              <h3>Vaults</h3>
-              <h3 className="ml-auto">Voting Power</h3>
-            </div>
-            <div className="flex max-h-64 flex-col gap-y-3 overflow-y-auto pr-8">
-              <div className="flex">
-                <h3 className="underline">Locking Vault</h3>
-                <h3 className="ml-auto">50,000</h3>
-              </div>
-
-              <div className="flex">
-                <h3 className="underline">Locking Vault</h3>
-                <h3 className="ml-auto">50,000</h3>
-              </div>
-
-              <div className="flex">
-                <h3 className="underline">Locking Vault</h3>
-                <h3 className="ml-auto">50,000</h3>
-              </div>
-
-              <div className="flex">
-                <h3 className="underline">Locking Vault</h3>
-                <h3 className="ml-auto">50,000</h3>
-              </div>
-
-              <div className="flex">
-                <h3 className="underline">Locking Vault</h3>
-                <h3 className="ml-auto">50,000</h3>
-              </div>
-
-              <div className="flex">
-                <h3 className="underline">Locking Vault</h3>
-                <h3 className="ml-auto">50,000</h3>
-              </div>
-
-              <div className="flex">
-                <h3 className="underline">Locking Vault</h3>
-                <h3 className="ml-auto">50,000</h3>
-              </div>
-
-              <div className="flex">
-                <h3 className="underline">Locking Vault</h3>
-                <h3 className="ml-auto">50,000</h3>
-              </div>
-
-              <div className="flex">
-                <h3 className="underline">Locking Vault</h3>
-                <h3 className="ml-auto">50,000</h3>
-              </div>
-
-              <div className="flex">
-                <h3 className="underline">Locking Vault</h3>
-                <h3 className="ml-auto">50,000</h3>
-              </div>
-
-              <div className="flex">
-                <h3 className="underline">Locking Vault</h3>
-                <h3 className="ml-auto">50,000</h3>
-              </div>
-            </div>
-
-            <div className="flex">
-              <h2 className="text-lg text-secondary-content">
-                Total Voting Power
-              </h2>
-              <h2 className="ml-auto text-lg font-bold text-accent-content">
-                200,000
-              </h2>
-            </div>
-
-            <div className="daisy-btn-group m-auto">
-              <button className="daisy-btn daisy-btn-active daisy-btn-lg">
-                YES
-              </button>
-              <button className="daisy-btn daisy-btn-lg">NO</button>
-              <button className="daisy-btn daisy-btn-lg">ABSTAIN</button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
 }
