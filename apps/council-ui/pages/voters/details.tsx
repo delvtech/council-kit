8a151f8ddf9f7a7f388fe0c6d7556e14ce4dafcd
Cash DeLeon <cashd@users.noreply.github.com>
2022-11-23 12:48:26 -0600

    wip

--- apps/council-ui/pages/voters/details.tsx
+++ apps/council-ui/pages/voters/details.tsx
@@ -1,25 +1,54 @@
 import { sumStrings, Vote, Voter, VoteResults } from "@council/sdk";
 import { useQuery } from "@tanstack/react-query";
+import assertNever from "assert-never";
 import { formatUnits, parseEther } from "ethers/lib/utils";
 import { useRouter } from "next/router";
 import { ReactElement } from "react";
 import { formatAddress } from "src/ui/base/formatting/formatAddress";
 import { formatBalance } from "src/ui/base/formatting/formatBalance";
 import { Page } from "src/ui/base/Page";
+import { Progress } from "src/ui/base/Progress";
 import { useCouncil } from "src/ui/council/useCouncil";
 import { formatGSCStatus } from "src/ui/utils/formatGSCStatus";
 
+interface VoterDataByVault {
+  name: string;
+  votingPower: string;
+}
+
+interface VoterData {
+  isGSC: boolean | undefined;
+  proposalsVoted: number;
+  voter: Voter;
+  voterDataByVault: VoterDataByVault[];
+  votingHistory: Vote[];
+  votingPower: string;
+}
+
 function useVoterData(address: string | undefined) {
   const { coreVoting, context, gscVoting } = useCouncil();
-  return useQuery(
+
+  return useQuery<VoterData>(
     ["voter", address],
     async () => {
       const voter = new Voter(address as string, context);
+
       const votingHistory = await voter.getVotes(coreVoting.address);
       const proposalsVoted = votingHistory.length;
-
       const votingPower = await voter.getVotingPower(
         coreVoting.vaults.map((vault) => vault.address),
+      );
+
+      const voterDataByVault = await Promise.all(
+        coreVoting.vaults.map(async (vault) => {
+          const name = vault.name;
+          const votingPower = await vault.getVotingPower(address as string);
+          // TODO @cashd: fetch current delegation and num wallets delegated data
+          return {
+            name,
+            votingPower,
+          };
+        }),
       );
 
       const isGSC = await gscVoting?.getIsMember(address as string);
@@ -30,6 +59,7 @@ function useVoterData(address: string | undefined) {
         votingPower,
         proposalsVoted,
         isGSC,
+        voterDataByVault,
       };
     },
     {
@@ -42,7 +72,7 @@ export default function VoterDetailsPage(): ReactElement {
   const { query } = useRouter();
   const { address } = query as { address: string | undefined };
 
-  const { data: voterData } = useVoterData(address);
+  const { data: voterData, status, error } = useVoterData(address);
 
   return (
     <Page>
@@ -55,27 +85,52 @@ export default function VoterDetailsPage(): ReactElement {
         )}
       </div>
 
-      {voterData && (
-        <VoterStatisticsRow
-          voterAddress={voterData.voter.address}
-          votingPower={voterData.votingPower}
-          proposalsVoted={voterData.proposalsVoted}
-        />
-      )}
+      {(() => {
+        switch (status) {
+          case "loading":
+            return (
+              <div className="flex flex-col items-center gap-8 ">
+                <p>Loading voter data. This might take a while...</p>
+                <Progress />
+              </div>
+            );
+          case "error":
+            return (
+              <div className="daisy-mockup-code">
+                <code className="block whitespace-pre-wrap px-6 text-error">
+                  {error ? (error as any).toString() : "Unknown error"}
+                </code>
+              </div>
+            );
 
-      <div className="flex w-full flex-col gap-y-8 md:flex-row md:gap-x-4 md:gap-y-0">
-        {voterData && (
-          <div className="flex min-w-[500px] flex-col gap-y-4 sm:basis-[65%]">
-            <h2 className="text-2xl font-bold">Voting History</h2>
-            <VoterVoteHistoryList history={voterData.votingHistory} />
-          </div>
-        )}
+          case "success":
+            return (
+              <>
+                <VoterStatisticsRow
+                  voterAddress={voterData.voter.address}
+                  votingPower={voterData.votingPower}
+                  proposalsVoted={voterData.proposalsVoted}
+                />
 
-        <div className="flex flex-col gap-y-4 sm:basis-[35%]">
-          <div className="text-2xl font-bold">Voting Vault (6)</div>
-          <VoterVotingPower />
-        </div>
-      </div>
+                <div className="flex w-full flex-col gap-y-8 md:flex-row md:gap-x-4 md:gap-y-0">
+                  <div className="flex min-w-[500px] flex-col gap-y-4 sm:basis-[65%]">
+                    <h2 className="text-2xl font-bold">Voting History</h2>
+                    <VoterVoteHistoryList history={voterData.votingHistory} />
+                  </div>
+
+                  <div className="flex flex-col gap-y-4 sm:basis-[35%]">
+                    <div className="text-2xl font-bold">Voting Vault (6)</div>
+                    <VotingPowerByVault
+                      vaultData={voterData.voterDataByVault}
+                    />
+                  </div>
+                </div>
+              </>
+            );
+          default:
+            assertNever(status);
+        }
+      })()}
     </Page>
   );
 }
@@ -115,13 +170,6 @@ function VoterStatisticsRow({
         </div>
       </div>
 
-      {/* <div className="daisy-stats">
-        <div className="daisy-stat bg-base-300">
-          <div className="daisy-stat-title">% of TVP</div>
-          <div className="daisy-stat-value text-sm">12%</div>
-        </div>
-      </div> */}
-
       <div className="daisy-stats">
         <div className="daisy-stat bg-base-300">
           <div className="daisy-stat-title">GSC Member</div>
@@ -135,13 +183,6 @@ function VoterStatisticsRow({
           <div className="daisy-stat-value text-sm">{proposalsVoted}</div>
         </div>
       </div>
-
-      {/* <div className="daisy-stats">
-        <div className="daisy-stat bg-base-300">
-          <div className="daisy-stat-title">Participants</div>
-          <div className="daisy-stat-value text-sm">2.112</div>
-        </div>
-      </div> */}
     </div>
   );
 }
@@ -209,8 +250,6 @@ function VoteResultBar({ results }: VoteResultBarProps) {
 
   const yesPercent =
     +formatUnits(parseEther(yesResult).div(resultsTotal), 0) * 100;
-  // const noPercent =
-  //   +formatUnits(parseEther(noResult).div(resultsTotal), 0) * 100;
   const maybePercent =
     +formatUnits(parseEther(maybeResult).div(resultsTotal), 0) * 100;
 
@@ -244,88 +283,38 @@ function VoteResultBar({ results }: VoteResultBarProps) {
   );
 }
 
-function VoterVotingPower(): ReactElement {
+interface VotingPowerByVaultProps {
+  vaultData: VoterDataByVault[];
+}
+
+function VotingPowerByVault({
+  vaultData,
+}: VotingPowerByVaultProps): ReactElement {
   return (
     <div className="flex h-96 flex-col gap-y-4 overflow-auto pr-3">
-      <div className="flex flex-col gap-y-2">
-        <h3 className="text-xl font-bold underline">Locking Vault</h3>
-        <div className="flex">
-          <p>Tokens Deposited</p>
-          <p className="ml-auto">6,000 ELFI</p>
-        </div>
-        <div className="flex">
-          <p>Voting Power</p>
-          <p className="ml-auto">1,000 ELFI</p>
-        </div>
-        <div className="flex">
-          <p>Current Delegation</p>
-          <p className="ml-auto">Self</p>
-        </div>
-        <div className="flex">
-          <p>Wallets Delegated</p>
-          <p className="ml-auto underline">12</p>
-        </div>
-      </div>
-
-      <div className="flex flex-col gap-y-2">
-        <h3 className="text-lg font-bold underline">Locking Vault</h3>
-        <div className="flex">
-          <p>Tokens Deposited</p>
-          <p className="ml-auto">6,000 ELFI</p>
-        </div>
-        <div className="flex">
-          <p>Voting Power</p>
-          <p className="ml-auto">1,000 ELFI</p>
-        </div>
-        <div className="flex">
-          <p>Current Delegation</p>
-          <p className="ml-auto">Self</p>
-        </div>
-        <div className="flex">
-          <p>Wallets Delegated</p>
-          <p className="ml-auto underline">12</p>
-        </div>
-      </div>
-
-      <div className="flex flex-col gap-y-2">
-        <h3 className="text-lg font-bold underline">Locking Vault</h3>
-        <div className="flex">
-          <p>Tokens Deposited</p>
-          <p className="ml-auto">6,000 ELFI</p>
-        </div>
-        <div className="flex">
-          <p>Voting Power</p>
-          <p className="ml-auto">1,000 ELFI</p>
-        </div>
-        <div className="flex">
-          <p>Current Delegation</p>
-          <p className="ml-auto">Self</p>
-        </div>
-        <div className="flex">
-          <p>Wallets Delegated</p>
-          <p className="ml-auto underline">12</p>
-        </div>
-      </div>
-
-      <div className="flex flex-col gap-y-2">
-        <h3 className="text-lg font-bold underline">Locking Vault</h3>
-        <div className="flex">
-          <p>Tokens Deposited</p>
-          <p className="ml-auto">6,000 ELFI</p>
-        </div>
-        <div className="flex">
-          <p>Voting Power</p>
-          <p className="ml-auto">1,000 ELFI</p>
-        </div>
-        <div className="flex">
-          <p>Current Delegation</p>
-          <p className="ml-auto">Self</p>
-        </div>
-        <div className="flex">
-          <p>Wallets Delegated</p>
-          <p className="ml-auto underline">12</p>
-        </div>
-      </div>
+      {vaultData.map((vault) => {
+        return (
+          <div className="flex flex-col gap-y-2" key={vault.name}>
+            <h3 className="text-xl font-bold underline">{vault.name}</h3>
+            {/* <div className="flex">
+              <p>Tokens Deposited</p>
+              <p className="ml-auto">6,000 ELFI</p>
+            </div> */}
+            <div className="flex">
+              <p>Voting Power</p>
+              <p className="ml-auto">{formatBalance(vault.votingPower)} ELFI</p>
+            </div>
+            {/* <div className="flex">
+              <p>Current Delegation</p>
+              <p className="ml-auto">Self</p>
+            </div>
+            <div className="flex">
+              <p>Wallets Delegated</p>
+              <p className="ml-auto underline">12</p>
+            </div> */}
+          </div>
+        );
+      })}
     </div>
   );
 }
