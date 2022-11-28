775d0f645ba3349f246be24e2c06ec02a8ea99d0
Cash DeLeon <cashd@users.noreply.github.com>
2022-11-28 16:18:42 -0600

    Create getFormattedGSCStatus.ts

---
+++ apps/council-ui/src/ui/voter/utils/getFormattedGSCStatus.ts
@@ -0,0 +1,13 @@
+import { GSCVotingContract } from "@council/sdk";
+import { formatGSCStatus, GSCStatus } from "src/ui/utils/formatGSCStatus";
+
+export async function getFormattedGSCStatus(
+  address: string,
+  gscVoting: GSCVotingContract,
+): Promise<GSCStatus> {
+  const isIdle = await gscVoting?.getIsIdle(address as string);
+  const isMember = await gscVoting?.getIsMember(address as string);
+  const isEligible = await gscVoting?.getIsEligible(address as string);
+
+  return formatGSCStatus(isIdle, isMember, isEligible);
+}
