90619f473f31cba6c68eec46ae5c75f06b2805fe
Cash DeLeon <cashd@users.noreply.github.com>
2022-11-28 23:19:04 -0600

    add address key to hook

--- apps/council-ui/src/ui/voter/hooks/useVoterDataByVault.ts
+++ apps/council-ui/src/ui/voter/hooks/useVoterDataByVault.ts
@@ -15,47 +15,51 @@ export function useVoterDataByVault(
 ): UseQueryResult<VoterDataByVault[]> {
   const { coreVoting } = useCouncil();
 
-  return useQuery<VoterDataByVault[]>(["voter-data-by-vault"], async () => {
-    const _address = address as string;
+  return useQuery<VoterDataByVault[]>(
+    ["voter-data-by-vault", address],
+    async () => {
+      const _address = address as string;
 
-    return Promise.all(
-      coreVoting.vaults.map(async (vault) => {
-        const name = vault.name;
-        const votingPower = await vault.getVotingPower(_address);
+      return Promise.all(
+        coreVoting.vaults.map(async (vault) => {
+          const name = vault.name;
+          const votingPower = await vault.getVotingPower(_address);
 
-        if (vault instanceof LockingVault) {
-          const balance = await vault.getDepositedBalance(_address);
-          const numDelegated = (await vault.getDelegatorsTo(_address)).length;
-          const currentDelegate = await vault.getDelegate(_address);
+          if (vault instanceof LockingVault) {
+            const balance = await vault.getDepositedBalance(_address);
+            const numDelegated = (await vault.getDelegatorsTo(_address)).length;
+            const currentDelegate = await vault.getDelegate(_address);
+
+            return {
+              name,
+              votingPower,
+              balance,
+              numDelegated,
+              currentDelegate,
+            };
+          }
+
+          if (vault instanceof VestingVault) {
+            const balance = await (await vault.getGrant(_address)).votingPower;
+            const numDelegated = (await vault.getDelegatorsTo(_address)).length;
+            const currentDelegate = await vault.getDelegate(_address);
+
+            return {
+              name,
+              votingPower,
+              balance,
+              numDelegated,
+              currentDelegate,
+            };
+          }
 
           return {
             name,
             votingPower,
-            balance,
-            numDelegated,
-            currentDelegate,
           };
-        }
-
-        if (vault instanceof VestingVault) {
-          const balance = await (await vault.getGrant(_address)).votingPower;
-          const numDelegated = (await vault.getDelegatorsTo(_address)).length;
-          const currentDelegate = await vault.getDelegate(_address);
-
-          return {
-            name,
-            votingPower,
-            balance,
-            numDelegated,
-            currentDelegate,
-          };
-        }
-
-        return {
-          name,
-          votingPower,
-        };
-      }),
-    );
-  });
+        }),
+      );
+    },
+    {},
+  );
 }
