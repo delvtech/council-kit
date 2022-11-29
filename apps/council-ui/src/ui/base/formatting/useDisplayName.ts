9d2fd3ea488aa4636346f9678ad7bad434b27d66
Ryan Goree <goree.ryan@gmail.com>
2022-11-29 15:12:40 -0600

    Accept undefined in useDisplayName

--- apps/council-ui/src/ui/base/formatting/useDisplayName.ts
+++ apps/council-ui/src/ui/base/formatting/useDisplayName.ts
@@ -1,7 +1,11 @@
 import { useEnsName } from "wagmi";
 import { formatAddress } from "./formatAddress";
 
-export function useDisplayName(address: string): string {
-  const { data: ensName } = useEnsName({ address: address as `0x{string}` });
-  return ensName || formatAddress(address);
+export function useDisplayName(
+  address: string | undefined,
+): string | undefined {
+  const { data: ensName } = useEnsName({
+    address: address as `0x{string}` | undefined,
+  });
+  return ensName || (address && formatAddress(address));
 }
