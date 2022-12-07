5882d585ea3a4b36093412eb575fe267823b8d3f
Cash DeLeon <cashd@users.noreply.github.com>
2022-12-07 14:59:14 -0600

    change colors

--- apps/council-ui/pages/index.tsx
+++ apps/council-ui/pages/index.tsx
@@ -6,7 +6,7 @@ export default function Index(): ReactElement {
   return (
     <div className="space-y-8 m-auto max-w-3xl mt-16 p-10">
       <h1 className="text-5xl text-accent-content">
-        <span className="text-primary">Council</span> is a decentralized
+        <span className="text-accent">Council</span> is a decentralized
         governance system that allows a community to manage a DAO.
       </h1>
       <p className="text-2xl">
@@ -17,7 +17,7 @@ export default function Index(): ReactElement {
       </p>
       <div className="space-x-4">
         <Link href="/proposals">
-          <button className="daisy-btn daisy-btn-primary">Open App</button>
+          <button className="daisy-btn daisy-btn-accent">Open App</button>
         </Link>
         <a
           href="https://github.com/element-fi/council-monorepo"
