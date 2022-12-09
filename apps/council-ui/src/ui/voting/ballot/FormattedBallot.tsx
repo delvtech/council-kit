3ca6414cc5a49a96df68a28b47be2c386552c56e
Cash DeLeon <cashd@users.noreply.github.com>
2022-12-09 16:26:28 -0600

    Update FormattedBallot.tsx

--- apps/council-ui/src/ui/voting/ballot/FormattedBallot.tsx
+++ apps/council-ui/src/ui/voting/ballot/FormattedBallot.tsx
@@ -10,16 +10,12 @@ export default function FormattedBallot({
 }: FormattedBallotProps): ReactElement {
   switch (ballot) {
     case "yes":
-      return (
-        <h2 className="text-success font-semibold">{ballot.toUpperCase()}</h2>
-      );
+      return <h2 className="text-success font-semibold uppercase">{ballot}</h2>;
 
     case "no":
-      return (
-        <h2 className="text-error font-semibold">{ballot.toUpperCase()}</h2>
-      );
+      return <h2 className="text-error font-semibold uppercase">{ballot}</h2>;
 
     case "maybe":
-      return <h2 className="font-semibold">{ballot.toUpperCase()}</h2>;
+      return <h2 className="font-semibold uppercase">{ballot}</h2>;
   }
 }
