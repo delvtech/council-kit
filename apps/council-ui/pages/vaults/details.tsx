3b0f4b2f513b40bdb94f1fb50fc7e38758c3b54b
Cash DeLeon <cashd@users.noreply.github.com>
2022-12-12 14:34:47 -0600

    remove logic from vaults details page root and cleanup

--- apps/council-ui/pages/vaults/details.tsx
+++ apps/council-ui/pages/vaults/details.tsx
@@ -1,223 +1,48 @@
-import { LockingVault, VestingVault, VotingVault } from "@council/sdk";
-import { useQuery, UseQueryResult } from "@tanstack/react-query";
 import assertNever from "assert-never";
 import { useRouter } from "next/router";
 import { ReactElement } from "react";
-import { councilConfigs, SupportedChainId } from "src/config/council.config";
-import { VaultConfig } from "src/config/CouncilConfig";
-import { chains } from "src/provider";
-import { formatBalance } from "src/ui/base/formatting/formatBalance";
+import { councilConfigs } from "src/config/council.config";
 import { Page } from "src/ui/base/Page";
-import { ExternalLinkSVG } from "src/ui/base/svg/ExternalLink";
-import { useCouncil } from "src/ui/council/useCouncil";
+import { useChainId } from "src/ui/network/useChainId";
+import { useVaultConfig } from "src/ui/vaults/hooks/useVaultConfig";
 import { LockingVaultDetails } from "src/ui/vaults/LockingVaultDetails";
 import { VestingVaultDetails } from "src/ui/vaults/VestingVaultDetails";
-import { useAccount, useNetwork } from "wagmi";
 
 export default function Vault(): ReactElement {
-  const { address: account } = useAccount();
-  const { query } = useRouter();
-  const { data, isLoading, isError, error } = useVaultDetailsData(
-    query.address as string,
-    account,
+  const {
+    query: { address },
+  } = useRouter();
+
+  const chainId = useChainId();
+  const vaultConfig = useVaultConfig(
+    address as string | undefined,
+    councilConfigs[chainId].coreVoting,
   );
+
+  if (!address || !vaultConfig?.type) {
+    return <Page>could not find vault by address</Page>;
+  }
+
   return (
-    <Page>
-      {isError ? (
-        <div className="daisy-mockup-code">
-          <code className="block whitespace-pre-wrap px-6 text-error">
-            {error ? (error as any).toString() : "Unknown error"}
-          </code>
-        </div>
-      ) : isLoading ? (
-        <progress className="daisy-progress m-auto w-56 items-center"></progress>
-      ) : (
-        <>
-          <div>
-            {data.descriptionURL ? (
-              <a href={data.descriptionURL} target="_blank" rel="noreferrer">
-                <h1 className="text-5xl font-bold">
-                  {data.name} <ExternalLinkSVG size={30} />
-                </h1>
-              </a>
-            ) : (
-              <h1 className="text-5xl font-bold">{data.name}</h1>
-            )}
-          </div>
+    <div>
+      {(() => {
+        switch (vaultConfig?.type) {
+          case "LockingVault":
+            return <LockingVaultDetails address={address as string} />;
 
-          <div className="flex flex-wrap gap-4">
-            {typeof data.activeProposalCount === "number" && (
-              <div className="daisy-stats">
-                <div className="daisy-stat bg-base-300">
-                  <div className="daisy-stat-title">Active Proposals</div>
-                  <div className="daisy-stat-value text-sm">
-                    {data.activeProposalCount}
-                  </div>
-                </div>
-              </div>
-            )}
+          case "VestingVault":
+            return <VestingVaultDetails address={address as string} />;
 
-            {data.accountVotingPower && (
-              <div className="daisy-stats">
-                <div className="daisy-stat bg-base-300">
-                  <div className="daisy-stat-title">Your Voting Power</div>
-                  <div className="daisy-stat-value text-sm">
-                    {formatBalance(data.accountVotingPower)}
-                  </div>
-                </div>
-              </div>
-            )}
+          case "GSCVault":
+            return <></>;
 
-            {typeof data.accountPercentOfTVP === "number" && (
-              <div className="daisy-stats">
-                <div className="daisy-stat bg-base-300">
-                  <div className="daisy-stat-title">% of Total TVP</div>
-                  <div className="daisy-stat-value text-sm">
-                    {formatBalance(data.accountPercentOfTVP, 2)}%
-                  </div>
-                </div>
-              </div>
-            )}
+          case undefined:
+            return <></>;
 
-            {typeof data.delegatedToAccount === "number" && (
-              <div className="daisy-stats">
-                <div className="daisy-stat bg-base-300">
-                  <div className="daisy-stat-title">Delegated to You</div>
-                  <div className="daisy-stat-value text-sm">
-                    {data.delegatedToAccount}
-                  </div>
-                </div>
-              </div>
-            )}
-
-            {typeof data.participants === "number" && (
-              <div className="daisy-stats">
-                <div className="daisy-stat bg-base-300">
-                  <div className="daisy-stat-title">Participants</div>
-                  <div className="daisy-stat-value text-sm">
-                    {data.participants}
-                  </div>
-                </div>
-              </div>
-            )}
-          </div>
-
-          {account &&
-            query.address &&
-            (() => {
-              switch (data.type) {
-                case "LockingVault":
-                  return (
-                    <LockingVaultDetails
-                      account={account}
-                      address={query.address as string}
-                    />
-                  );
-
-                case "VestingVault":
-                  return (
-                    <VestingVaultDetails
-                      account={account}
-                      address={query.address as string}
-                    />
-                  );
-
-                case "GSCVault":
-                  return <></>;
-
-                case undefined:
-                  return <></>;
-
-                default:
-                  assertNever(data.type);
-              }
-            })()}
-        </>
-      )}
-    </Page>
-  );
-}
-
-interface VaultDetailsData {
-  name: string;
-  type?: VaultConfig["type"];
-  descriptionURL?: string;
-  activeProposalCount?: number;
-  accountVotingPower?: string;
-  accountPercentOfTVP?: number;
-  delegatedToAccount?: number;
-  participants?: number;
-}
-
-function useVaultDetailsData(
-  address: string,
-  account: string | undefined,
-): UseQueryResult<VaultDetailsData> {
-  const { context, coreVoting, gscVoting } = useCouncil();
-  const { chain } = useNetwork();
-  const chainId = chain?.id ?? chains[0].id;
-
-  return useQuery({
-    queryKey: ["vaultDetails", address, account, chainId],
-    queryFn: async () => {
-      const config = councilConfigs[chainId as SupportedChainId];
-
-      let vault = coreVoting.vaults.find((vault) => vault.address === address);
-      let votingContract = vault && coreVoting;
-      let votingContractConfig = vault && config.coreVoting;
-
-      if (!vault && gscVoting && gscVoting?.vaults[0].address === address) {
-        votingContract = gscVoting;
-        vault = gscVoting.vaults[0];
-        votingContractConfig = config.gscVoting;
-      }
-
-      if (!vault) {
-        vault = new VotingVault(address, context);
-      }
-
-      let delegatedToAccount: number | undefined;
-      if (
-        account &&
-        (vault instanceof LockingVault || vault instanceof VestingVault)
-      ) {
-        delegatedToAccount = (await vault.getDelegatorsTo(account)).length;
-      }
-
-      let activeProposalCount: number | undefined;
-      const proposals = await votingContract?.getProposals();
-      if (proposals) {
-        activeProposalCount = 0;
-        for (const proposal of proposals) {
-          if (await proposal.getIsActive()) {
-            activeProposalCount++;
-          }
+          default:
+            assertNever(vaultConfig?.type);
         }
-      }
-
-      const type =
-        votingContractConfig &&
-        votingContractConfig.vaults.find(
-          (vaultConfig) => vaultConfig.address === address,
-        )?.type;
-      const accountVotingPower =
-        account && (await vault.getVotingPower(account));
-
-      return {
-        name: vault.name,
-        type,
-        descriptionURL: votingContractConfig?.vaults.find(
-          (vault) => vault.address === address,
-        )?.descriptionURL,
-        activeProposalCount,
-        accountVotingPower,
-        accountPercentOfTVP:
-          accountVotingPower && vault.getTotalVotingPower
-            ? (+accountVotingPower / +(await vault.getTotalVotingPower())) * 100
-            : undefined,
-        delegatedToAccount,
-        participants: vault.getVoters && (await vault.getVoters()).length,
-      };
-    },
-  });
+      })()}
+    </div>
+  );
 }
