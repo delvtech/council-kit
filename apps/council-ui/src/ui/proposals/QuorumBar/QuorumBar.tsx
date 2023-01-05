08228ec4036b8a546d790085bc3f15aad97d5789
Cash DeLeon <cashd@users.noreply.github.com>
2023-01-05 14:11:21 -0600

    refactored qurom bar and added skeleton

--- apps/council-ui/src/ui/proposals/QuorumBar/QuorumBar.tsx
+++ apps/council-ui/src/ui/proposals/QuorumBar/QuorumBar.tsx
@@ -1,4 +1,6 @@
+import { FixedNumber } from "ethers";
 import { ReactElement } from "react";
+import Skeleton from "react-loading-skeleton";
 import { formatBalance } from "src/ui/base/formatting/formatBalance";
 
 interface QuorumBarProps {
@@ -6,23 +8,40 @@ interface QuorumBarProps {
   required: string;
 }
 
-export default function QuorumBar({
-  current,
-  required,
-}: QuorumBarProps): ReactElement {
+export function QuorumBar({ current, required }: QuorumBarProps): ReactElement {
+  const percentage = FixedNumber.from(current)
+    .divUnsafe(FixedNumber.from(required))
+    .mulUnsafe(FixedNumber.from(100))
+    .round();
+
   return (
     <div>
-      <div className="flex w-full">
-        <h3>QUORUM</h3>
-        <p className="ml-auto font-bold">
-          {formatBalance(current)} / {formatBalance(required)}
-        </p>
+      <div className="flex ">
+        <h3 className="mr-6 font-medium">QUORUM</h3>
+        <p className="ml-auto font-bold">{percentage.toString()} %</p>
       </div>
       <progress
-        className="daisy-progress daisy-progress-info w-48 bg-neutral sm:w-64"
+        className="w-full daisy-progress daisy-progress-info bg-neutral"
         value={current}
         max={required}
-      ></progress>
+      />
+      <div className="flex flex-row-reverse gap-x-1">
+        <p className="font-bold">{formatBalance(current)}</p> <span>/</span>
+        <p> {formatBalance(required)}</p>
+      </div>
+    </div>
+  );
+}
+
+// ================ Skeletons ================
+
+export function QuorumBarSkeleton(): ReactElement {
+  return (
+    <div>
+      <div className="flex ">
+        <h3 className="w-24 mr-6 font-medium">QUORUM</h3>
+      </div>
+      <Skeleton />
     </div>
   );
 }
