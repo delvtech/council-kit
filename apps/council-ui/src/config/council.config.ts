c966224bf9031a835cc5fb20cd40d3b6ee38451c
Danny Delott <dannydelott@gmail.com>
2023-01-04 19:16:18 -0800

    create local variables, add comment

--- apps/council-ui/src/config/council.config.ts
+++ apps/council-ui/src/config/council.config.ts
@@ -65,32 +65,50 @@ const mainnetCouncilConfig: CouncilConfig = {
 const { contracts: goerliContracts } =
   goerliDeployments[goerliDeployments.length - 1];
 
+// Find the deployed contract addresses. These are safe to cast as strings
+// because we know the deployment contains these contracts in the
+// @council/deploy project.
+const goerliTimelockAddress = goerliContracts.find(
+  ({ name }) => name === "Timelock",
+)?.address as string;
+const goerliCoreVotingAddress = goerliContracts.find(
+  ({ name }) => name === "CoreVoting",
+)?.address as string;
+const goerliLockingVaultAddress = goerliContracts.find(
+  ({ name }) => name === "LockingVault",
+)?.address as string;
+const goerliVestingVaultAddress = goerliContracts.find(
+  ({ name }) => name === "VestingVault",
+)?.address as string;
+const goerliGSCVotingAddress = goerliContracts.find(
+  ({ name }) => name === "GSCCoreVoting",
+)?.address as string;
+const goerliGSCVaultAddress = goerliContracts.find(
+  ({ name }) => name === "GSCVault",
+)?.address as string;
+
 const goerliCouncilConfig: CouncilConfig = {
   version: "",
   chainId: 5,
   timelock: {
-    address: goerliContracts.find(({ name }) => name === "Timelock")
-      ?.address as string,
+    address: goerliTimelockAddress,
     abi: {},
   },
   coreVoting: {
-    address: goerliContracts.find(({ name }) => name === "CoreVoting")
-      ?.address as string,
+    address: goerliCoreVotingAddress,
     abi: {},
     descriptionURL: "https://moreinfo.com",
     vaults: [
       {
         name: "Locking Vault",
-        address: goerliContracts.find(({ name }) => name === "LockingVault")
-          ?.address as string,
+        address: goerliLockingVaultAddress,
         type: "LockingVault",
         abi: {},
         descriptionURL: "https://moreinfo.com",
       },
       {
         name: "Vesting Vault",
-        address: goerliContracts.find(({ name }) => name === "VestingVault")
-          ?.address as string,
+        address: goerliVestingVaultAddress,
         type: "VestingVault",
         abi: {},
         descriptionURL: "https://moreinfo.com",
@@ -111,15 +129,13 @@ const goerliCouncilConfig: CouncilConfig = {
   },
 
   gscVoting: {
-    address: goerliContracts.find(({ name }) => name === "GSCCoreVoting")
-      ?.address as string,
+    address: goerliGSCVotingAddress,
     abi: {},
     descriptionURL: "https://moreinfo.com",
     vaults: [
       {
         name: "GSC Vault",
-        address: goerliContracts.find(({ name }) => name === "GSCVault")
-          ?.address as string,
+        address: goerliGSCVaultAddress,
         type: "GSCVault",
         abi: {},
         descriptionURL: "https://moreinfo.com",
