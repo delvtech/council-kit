455521d2c4474bf192793b49e9137ea4ec0362d7
Cash DeLeon <cashd@users.noreply.github.com>
2023-01-06 16:50:29 -0600

    wip

--- apps/council-ui/pages/index.tsx
+++ apps/council-ui/pages/index.tsx
@@ -4,7 +4,7 @@ import { ReactElement } from "react";
 
 export default function Index(): ReactElement {
   return (
-    <div className="space-y-8 m-auto max-w-3xl mt-16 p-10">
+    <div className="max-w-3xl p-10 m-auto mt-16 space-y-8">
       <h1 className="text-5xl text-accent-content">
         <span className="text-accent">Council</span> is a decentralized
         governance system that allows a community to manage a DAO.
@@ -15,6 +15,7 @@ export default function Index(): ReactElement {
         successfully maintaining the security and robustness of the governed
         protocol.
       </p>
+      a
       <div className="space-x-4">
         <Link href="/proposals">
           <button className="daisy-btn daisy-btn-accent">Open App</button>
