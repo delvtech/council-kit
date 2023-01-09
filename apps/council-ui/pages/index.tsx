4c588ae43c1b1316e4cb1f9f00d51fe5bbf82fd4
CPSTL <32653033+CPSTL@users.noreply.github.com>
2023-01-09 15:07:36 -0500

    Add copy to index.tsx
    Excuse the poor formatting

--- apps/council-ui/pages/index.tsx
+++ apps/council-ui/pages/index.tsx
@@ -8,13 +8,11 @@ export default function Index(): ReactElement {
       <div className="flex flex-col max-w-5xl p-24 m-auto gap-y-8">
         <h1 className="text-5xl text-accent-content">
           <span className="text-accent">Council</span> is a decentralized
-          governance system that allows a community to manage a DAO.
+          governance protocol that allows a community to manage a DAO.
         </h1>
         <p className="text-2xl">
-          The <span className="text-secondary">governance </span>system is
-          designed to enable flexibility, improvements, and experimentation
-          while successfully maintaining the security and robustness of the
-          governed protocol.
+          A <span className="text-secondary">governance </span>A governance system designed to be modular and adaptable.
+          Council is here to reinvigorate the standard model for governance that keeps decentralization at the forefront           and allows DAOs to scale decision making. 
         </p>
         <div className="space-x-4">
           <Link href="/proposals">
@@ -35,9 +33,7 @@ export default function Index(): ReactElement {
           <div className="flex flex-col items-center gap-8">
             <div className="text-4xl font-medium">Get Started</div>
             <div className="text-xl text-center">
-              Build Defi apps and tools on the largest crypto project on
-              Ethereum. Get started with quick start guides, protocol
-              documentation, a Javascript SDK, and fully open source code.{" "}
+              Get up and running with Council with our quick start guides, protocol documentation, a fully customizable reference UI, a Javascript SDK, and fully open source code. {" "}
             </div>
 
             <div className="daisy-mockup-code">
@@ -55,28 +51,31 @@ export default function Index(): ReactElement {
           <div className="flex flex-col items-center gap-8">
             <div className="text-4xl font-medium">Why use Council?</div>
             <div className="text-xl font-medium text-center">
-              Council is a decentralized governance system on the Ethereum
-              mainnet blockchain that allows a community to manage a DAO.{" "}
+              Council Protocol was created because there are no governance frameworks that exist today that meet the               realistic needs of day-to-day and long-term governance. {" "}
             </div>
             <div className="flex gap-8">
               <div className="w-48 daisy-card bg-primary text-primary-content">
                 <div className="daisy-card-body">
                   <h2 className="daisy-card-title">Voting Vaults</h2>
-                  <p>If a dog chews shoes whose shoes does he choose?</p>
+                  <p>Grow your DAOs Inclusivity. Assign voting power to any use case.</p>
                 </div>
               </div>
 
               <div className="w-48 daisy-card bg-primary text-primary-content">
                 <div className="daisy-card-body">
                   <h2 className="daisy-card-title">GSC</h2>
-                  <p>If a dog chews shoes whose shoes does he choose?</p>
+                  <p>Scale Decision Making with the Governance Steering Council, elected through a delegation                             threshold.</p>
                 </div>
               </div>
 
               <div className="w-48 daisy-card bg-primary text-primary-content">
                 <div className="daisy-card-body">
                   <h2 className="daisy-card-title">Core Voting</h2>
-                  <p>If a dog chews shoes whose shoes does he choose?</p>
+                  <p>This system begins with the core…and the core is all that is needed at the base layer. Nothing                          more, nothing less. The Core simply consists of: 
+                      - Defining the voting process for those with governance power.
+                      - Tracking proposals and counting voting power.
+                      - Retrieving user voting power from approved Voting Vaults.
+                  </p>
                 </div>
               </div>
             </div>
@@ -119,10 +118,7 @@ export default function Index(): ReactElement {
           Built with ❤️ and 🧠 by <span className="text-secondary">Delve</span>
         </div>
         <div className="text-lg">
-          Building RainbowKit has been an incredibly fun effort across many
-          people at Rainbow and our frens at other companies. We &apos; re
-          always looking to make RainbowKit better, so please let us know how we
-          can improve.
+          Building Council has been an absolute pleasure and we couldn't have done it without the feedback and support             from you all. We're always looking to make Council better, so please let us know how we can continue to                 improve or ask how you can start contributing. 
         </div>
       </div>
     </div>
