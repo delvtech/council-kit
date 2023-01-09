93ff481da39ed3540d7a781bf7e4e10a42f05c1c
Cash DeLeon <cashd@users.noreply.github.com>
2023-01-09 15:40:53 -0600

    Vault Index loading states and cleanup

--- apps/council-ui/src/ui/vaults/base/GenericVaultCard.tsx
+++ apps/council-ui/src/ui/vaults/base/GenericVaultCard.tsx
@@ -1,8 +1,9 @@
 import Link from "next/link";
 import { ReactElement } from "react";
+import Skeleton from "react-loading-skeleton";
 import { makeVaultURL } from "src/routes";
+import { Address } from "src/ui/base/Address";
 import { formatBalance } from "src/ui/base/formatting/formatBalance";
-import { makeImgSrc } from "src/ui/council/imgSrc";
 
 interface GenericVaultCardProps {
   address: string;
@@ -18,31 +19,60 @@ export function GenericVaultCard({
   votingPower,
 }: GenericVaultCardProps): ReactElement {
   return (
-    <div className="daisy-card bg-base-200 shadow md:w-[420px]">
-      <figure>
-        <img src={makeImgSrc("voting-vault-art.webp")} alt={`${name} art`} />
-      </figure>
+    <div className="w-full sm:max-w-[300px] daisy-card bg-base-300">
       <div className="daisy-card-body">
-        <h2 className="daisy-card-title">{name}</h2>
+        <h2 className="text-2xl daisy-card-title">{name}</h2>
+        <Address address={address} className="text-lg" />
 
-        <div className="daisy-card-actions justify-start">
-          <div className="flex flex-row items-start mr-auto gap-x-6">
-            {tvp && (
-              <div>
-                <h2>TVP</h2>
-                <p className="font-extrabold">{formatBalance(tvp)}</p>
-              </div>
-            )}
-            {votingPower && (
-              <div>
-                <h2>Your TVP</h2>
-                <p className="font-extrabold">{formatBalance(votingPower)}</p>
-              </div>
-            )}
+        <div className="flex-col daisy-card-actions gap-y-6">
+          <div className="flex flex-row items-start mr-auto text-lg gap-x-6">
+            <div>
+              <h2>TVP</h2>
+              <p className="font-bold">{tvp ? formatBalance(tvp) : "None"}</p>
+            </div>
+            <div>
+              <h2>Your TVP</h2>
+              <p className="font-bold">
+                {votingPower ? formatBalance(votingPower) : "None"}
+              </p>
+            </div>
           </div>
           <Link href={makeVaultURL(address)}>
-            <button className=" daisy-btn daisy-btn-primary">Open Vault</button>
+            <button className=" daisy-btn daisy-btn-primary">Open</button>
           </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export function GenericVaultCardSkeleton(): ReactElement {
+  return (
+    <div className="w-full sm:max-w-[300px] daisy-card bg-base-300">
+      <div className="daisy-card-body">
+        <h2 className="w-32">
+          <Skeleton />
+          <Skeleton />
+        </h2>
+
+        <div className="flex-col daisy-card-actions gap-y-6">
+          <div className="flex flex-row items-start mr-auto text-lg gap-x-6">
+            <div className="w-24">
+              <Skeleton />
+              <p className="font-bold">
+                <Skeleton />
+              </p>
+            </div>
+            <div className="w-24">
+              <Skeleton />
+              <p className="font-bold">
+                <Skeleton />
+              </p>
+            </div>
+          </div>
+          <button className="daisy-btn daisy-btn-disabled daisy-btn-primary">
+            Open
+          </button>
         </div>
       </div>
     </div>
