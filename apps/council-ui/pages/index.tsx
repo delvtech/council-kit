9e8fba46464ffb88e287495bfd6c156ac9bef69f
Cash DeLeon <cashd@users.noreply.github.com>
2023-01-11 10:17:40 -0600

    Update index.tsx

--- apps/council-ui/pages/index.tsx
+++ apps/council-ui/pages/index.tsx
@@ -6,13 +6,16 @@ export default function Index(): ReactElement {
   return (
     <div className="m-auto">
       <div className="flex flex-col max-w-5xl p-24 m-auto gap-y-8">
-        <h1 className="text-5xl text-accent-content">
+        <h1 className="text-5xl font-bold text-accent-content">
           <span className="text-accent">Council</span> is a decentralized
           governance protocol that allows a community to manage a DAO.
         </h1>
         <p className="text-2xl">
-          A <span className="text-secondary">governance </span>A governance system designed to be modular and adaptable.
-          Council is here to reinvigorate the standard model for governance that keeps decentralization at the forefront           and allows DAOs to scale decision making. 
+          A <span className="text-secondary">governance </span>A governance
+          system designed to be modular and adaptable. Council is here to
+          reinvigorate the standard model for governance that keeps
+          decentralization at the forefront and allows DAOs to scale decision
+          making.
         </p>
         <div className="space-x-4">
           <Link href="/proposals">
@@ -31,9 +34,11 @@ export default function Index(): ReactElement {
       <div className="w-full p-24 bg-base-300">
         <div className="max-w-5xl">
           <div className="flex flex-col items-center gap-8">
-            <div className="text-4xl font-medium">Get Started</div>
+            <div className="text-4xl font-bold">Get Started</div>
             <div className="text-xl text-center">
-              Get up and running with Council with our quick start guides, protocol documentation, a fully customizable reference UI, a Javascript SDK, and fully open source code. {" "}
+              Get up and running with Council with our quick start guides,
+              protocol documentation, a fully customizable reference UI, a
+              Javascript SDK, and fully open source code.{" "}
             </div>
 
             <div className="daisy-mockup-code">
@@ -47,34 +52,51 @@ export default function Index(): ReactElement {
 
       <div className="w-full p-24">
         <div className="max-w-5xl">
-          {" "}
           <div className="flex flex-col items-center gap-8">
-            <div className="text-4xl font-medium">Why use Council?</div>
-            <div className="text-xl font-medium text-center">
-              Council Protocol was created because there are no governance frameworks that exist today that meet the               realistic needs of day-to-day and long-term governance. {" "}
+            <div className="text-4xl font-bold">Why use Council?</div>
+            <div className="text-xl text-center">
+              Council Protocol was created because there are no governance
+              frameworks that exist today that meet the realistic needs of
+              day-to-day and long-term governance.
             </div>
-            <div className="flex gap-8">
-              <div className="w-48 daisy-card bg-primary text-primary-content">
+            <div className="flex flex-col max-w-md gap-4">
+              <div className="daisy-card bg-neutral text-neutral-content">
                 <div className="daisy-card-body">
-                  <h2 className="daisy-card-title">Voting Vaults</h2>
-                  <p>Grow your DAOs Inclusivity. Assign voting power to any use case.</p>
+                  <h2 className="daisy-card-title">Voting Vaults 🗳️</h2>
+                  <p>
+                    Grow your DAOs Inclusivity. Assign voting power to any use
+                    case.
+                  </p>
                 </div>
               </div>
 
-              <div className="w-48 daisy-card bg-primary text-primary-content">
+              <div className="daisy-card bg-neutral text-neutral-content">
                 <div className="daisy-card-body">
-                  <h2 className="daisy-card-title">GSC</h2>
-                  <p>Scale Decision Making with the Governance Steering Council, elected through a delegation                             threshold.</p>
+                  <h2 className="daisy-card-title">GSC 🏛️</h2>
+                  <p>
+                    Scale Decision Making with the Governance Steering Council,
+                    elected through a delegation threshold.
+                  </p>
                 </div>
               </div>
 
-              <div className="w-48 daisy-card bg-primary text-primary-content">
+              <div className="daisy-card bg-neutral text-neutral-content">
                 <div className="daisy-card-body">
-                  <h2 className="daisy-card-title">Core Voting</h2>
-                  <p>This system begins with the core…and the core is all that is needed at the base layer. Nothing                          more, nothing less. The Core simply consists of: 
-                      - Defining the voting process for those with governance power.
-                      - Tracking proposals and counting voting power.
-                      - Retrieving user voting power from approved Voting Vaults.
+                  <h2 className="daisy-card-title">Core Voting ⚖️</h2>
+                  <p>
+                    This system begins with the core…and the core is all that is
+                    needed at the base layer. Nothing more, nothing less.
+                    {/* <ul className="mt-1 list-disc">
+                      <li>
+                        Defining the voting process for those with governance
+                        power.
+                      </li>
+                      <li>Tracking proposals and counting voting power.</li>
+                      <li>
+                        Retrieving user voting power from approved Voting
+                        Vaults.
+                      </li>
+                    </ul> */}
                   </p>
                 </div>
               </div>
@@ -88,24 +110,40 @@ export default function Index(): ReactElement {
           <div className="flex flex-col items-center gap-8">
             <div className="text-4xl font-medium">Council 🤝 DAOs </div>
 
-            <div className="flex gap-8">
-              <div className="w-48 daisy-card bg-primary text-primary-content">
-                <div className="text-center daisy-card-body">
-                  <h2 className="daisy-card-title">Coordinape</h2>
-                  <div className="text-6xl">👻</div>
+            <div className="flex gap-6">
+              <div className="w-48 daisy-card bg-neutral text-neutral-content">
+                <div className="items-center text-center daisy-card-body">
+                  <h2 className="daisy-card-title">Yearn</h2>
+                  {/* <Image src={} /> */}
+                  <img
+                    alt="yearn logo"
+                    height={80}
+                    width={80}
+                    src="https://cryptologos.cc/logos/yearn-finance-yfi-logo.png"
+                  />
                 </div>
               </div>
-              <div className="w-48 daisy-card bg-primary text-primary-content">
-                <div className="text-center daisy-card-body">
-                  <h2 className="daisy-card-title">Coordinape</h2>
-                  <div className="text-6xl">👻</div>
+              <div className="w-48 daisy-card bg-neutral text-neutral-content">
+                <div className="items-center daisy-card-body">
+                  <h2 className="daisy-card-title">Balancer</h2>
+                  <img
+                    alt="balancer logo"
+                    height={80}
+                    width={80}
+                    src="https://cryptologos.cc/logos/balancer-bal-logo.svg?v=024"
+                  />
                 </div>
               </div>
 
-              <div className="w-48 daisy-card bg-primary text-primary-content">
-                <div className="text-center daisy-card-body">
-                  <h2 className="daisy-card-title">Coordinape</h2>
-                  <div className="text-6xl">👻</div>
+              <div className="w-48 daisy-card bg-neutral text-neutral-content">
+                <div className="items-center daisy-card-body">
+                  <h2 className="daisy-card-title">Synthetix</h2>
+                  <img
+                    alt="snx logo"
+                    height={80}
+                    width={80}
+                    src="https://cryptologos.cc/logos/synthetix-network-token-snx-logo.svg?v=024"
+                  />
                 </div>
               </div>
             </div>
@@ -118,7 +156,10 @@ export default function Index(): ReactElement {
           Built with ❤️ and 🧠 by <span className="text-secondary">Delve</span>
         </div>
         <div className="text-lg">
-          Building Council has been an absolute pleasure and we couldn't have done it without the feedback and support             from you all. We're always looking to make Council better, so please let us know how we can continue to                 improve or ask how you can start contributing. 
+          Building Council has been an absolute pleasure and we couldn&apos;t
+          have done it without the feedback and support from you all. We&apos;re
+          always looking to make Council better, so please let us know how we
+          can continue to improve or ask how you can start contributing.
         </div>
       </div>
     </div>
