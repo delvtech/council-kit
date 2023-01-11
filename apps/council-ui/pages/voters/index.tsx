41dac5bedaa37e7a0194efcaa5a40bfa16866bd7
Cash Baller <cashd@users.noreply.github.com>
2023-01-10 18:48:51 -0600

    Voter index loading states (#133)
    * Update index.tsx
    
    * move voter folder to voters
    
    * Voter index loading states and cleanup

--- apps/council-ui/pages/voters/index.tsx
+++ apps/council-ui/pages/voters/index.tsx
@@ -1,113 +1,67 @@
 import { useQuery, UseQueryResult } from "@tanstack/react-query";
-import { assertNever } from "assert-never";
-import Fuse from "fuse.js";
-import Link from "next/link";
-import { ReactElement, useEffect, useMemo, useRef, useState } from "react";
+import { ReactElement, useState } from "react";
 import { getBulkEnsRecords } from "src/ens/getBulkEnsRecords";
-import { makeVoterURL } from "src/routes";
+import { ErrorMessage } from "src/ui/base/error/ErrorMessage";
 import { Page } from "src/ui/base/Page";
-import { Progress } from "src/ui/base/Progress";
-import { WalletIcon } from "src/ui/base/WalletIcon";
 import { useCouncil } from "src/ui/council/useCouncil";
+import { useChainId } from "src/ui/network/useChainId";
+import { useVotersSearch } from "src/ui/voters/hooks/useVotersSearch";
+import { VoterRowData } from "src/ui/voters/types";
+import { VoterList, VoterListSkeleton } from "src/ui/voters/VoterList";
 
-const MAX_LIST_SIZE = 125;
+const DEFAULT_LIST_SIZE = 100;
 
 export default function Voters(): ReactElement {
-  const { data: voters, status, error, isLoading } = useVoterPageData();
-  const { results, search } = useVoterSearch(voters);
+  const { data: voters, status, error } = useVoterPageData();
+  const { results, search } = useVotersSearch(voters);
+  const [listSize, setListSize] = useState(DEFAULT_LIST_SIZE);
+
+  if (status === "error") {
+    return <ErrorMessage error={error} />;
+  }
 
   return (
     <Page>
       <div className="flex flex-col max-w-xl mx-auto gap-y-8">
-        <div className="flex w-full items-center gap-x-2">
-          <h1 className="w-full text-5xl text-center font-bold">Voters</h1>
+        <div className="flex flex-col">
+          <h1 className="text-5xl font-bold">Voters</h1>
+          <p className="mt-6 text-lg">
+            Voters are accounts that have participated in any on-chain vote
+            since the inception of the DAO. You can search by partial keywords
+            using ENS names or addresses.
+          </p>
         </div>
 
-        {/* Search Input Box */}
-        {!isLoading && (
+        {status === "success" ? (
           <input
             type="text"
-            placeholder="Search by ens or address"
-            className="daisy-input-bordered daisy-input w-full"
+            placeholder="Search by ENS or address"
+            className="w-full daisy-input-bordered daisy-input"
             onChange={(e) => {
               search(e.target.value as string);
             }}
           />
+        ) : (
+          <input
+            type="text"
+            placeholder="Search by ENS or address"
+            className="w-full daisy-input-bordered daisy-input"
+            disabled
+          />
         )}
 
-        {(() => {
-          switch (status) {
-            case "loading":
-              return (
-                <div className="flex flex-col items-center gap-8">
-                  <p>Loading voters. This might take a while...</p>
-                  <Progress />
-                </div>
-              );
-            case "error":
-              return (
-                <div className="daisy-mockup-code">
-                  <code className="block whitespace-pre-wrap px-6 text-error">
-                    {error ? (error as string).toString() : "Unknown error"}
-                  </code>
-                </div>
-              );
-
-            case "success":
-              return (
-                <table className="daisy-table-zebra daisy-table w-full min-w-[250px] mb-10 outline rounded-lg">
-                  {/* Table Header */}
-                  <thead>
-                    <tr>
-                      <th>Voter</th>
-                    </tr>
-                  </thead>
-
-                  {/* Table Body */}
-                  <tbody className="my-4">
-                    {results
-                      .slice(0, MAX_LIST_SIZE)
-                      .map(({ address, ensName }) => {
-                        return (
-                          <tr key={address}>
-                            <td className="underline sm:px-8 md:text-lg">
-                              <Link
-                                href={makeVoterURL(address)}
-                                className="flex items-center"
-                              >
-                                <WalletIcon
-                                  address={address}
-                                  className="mr-2"
-                                />
-                                {ensName ? ensName : address}
-                              </Link>
-                            </td>
-                          </tr>
-                        );
-                      })}
-                  </tbody>
-
-                  {results.length > MAX_LIST_SIZE && (
-                    <tfoot>
-                      <div className="py-8">
-                        Only loading the first {MAX_LIST_SIZE} voters. Please
-                        refine search to load more.
-                      </div>
-                    </tfoot>
-                  )}
-                </table>
-              );
-            default:
-              assertNever(status);
-          }
-        })()}
+        {status === "success" ? (
+          <VoterList
+            voters={results}
+            size={listSize}
+            onSizeChange={(newSize) => setListSize(newSize)}
+          />
+        ) : (
+          <VoterListSkeleton />
+        )}
       </div>
     </Page>
   );
-}
-interface VoterRowData {
-  address: string;
-  ensName: string | null;
 }
 
 function useVoterPageData(): UseQueryResult<VoterRowData[]> {
@@ -115,8 +69,10 @@ function useVoterPageData(): UseQueryResult<VoterRowData[]> {
     coreVoting,
     context: { provider },
   } = useCouncil();
+  const chainId = useChainId();
+
   return useQuery<VoterRowData[]>({
-    queryKey: ["voter-list-page"],
+    queryKey: ["voter-list-page", chainId],
     queryFn: async () => {
       const voters = await coreVoting.getVoters();
       const ensRecords = await getBulkEnsRecords(
@@ -130,44 +86,6 @@ function useVoterPageData(): UseQueryResult<VoterRowData[]> {
       }));
     },
     refetchOnWindowFocus: false,
+    staleTime: Infinity,
   });
-}
-
-const voterSearchFuseOptions = {
-  threshold: 0.3,
-  isCaseSensitive: false,
-  ignoreLocation: true,
-  keys: ["address", "ensName"],
-};
-
-function useVoterSearch(data: Array<VoterRowData> | undefined) {
-  const [input, setInput] = useState<string | null>(null);
-
-  const searchCache = useRef<Record<string, Array<VoterRowData>>>({});
-  useEffect(() => {
-    searchCache.current = {};
-  }, [data]);
-
-  const results = useMemo(() => {
-    const fuse = new Fuse(data ?? [], voterSearchFuseOptions);
-
-    if (input) {
-      if (searchCache.current[input]) {
-        return searchCache.current[input];
-      }
-
-      const filtered = fuse.search(input).map((item) => item.item);
-      searchCache.current[input] = filtered;
-
-      return filtered;
-    }
-
-    return data ?? [];
-  }, [input, data]);
-
-  return {
-    results,
-    reset: () => setInput(""),
-    search: (i: string) => setInput(i),
-  };
 }
---
+++ apps/council-ui/src/ui/voters/types.ts
@@ -0,0 +1,4 @@
+export interface VoterRowData {
+  address: string;
+  ensName: string | null;
+}
