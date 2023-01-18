13f5c800dc8d0de39c25b052ce3a81ea410ab487
Ryan Goree <goree.ryan@gmail.com>
2023-01-18 14:41:14 -0600

    Add a redirect to profile page to make the /profile route work

--- apps/council-ui/pages/profile/index.tsx
+++ apps/council-ui/pages/profile/index.tsx
@@ -1,5 +1,25 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { useRouter } from "next/router";
 import { ReactElement } from "react";
+import { Page } from "src/ui/base/Page";
+import { useAccount } from "wagmi";
 
-export default function Profile(): ReactElement {
-  return <>Profile</>;
+export default function Profile(): ReactElement | void {
+  const { address } = useAccount();
+  const { replace } = useRouter();
+
+  if (!address) {
+    return (
+      <Page>
+        <div className="text-center flex flex-col items-center gap-2">
+          <h1 className="text-xl font-bold">
+            Connect your wallet to view your profile.
+          </h1>
+          <ConnectButton />
+        </div>
+      </Page>
+    );
+  }
+
+  replace(`/voters/details?address=${address}`);
 }
