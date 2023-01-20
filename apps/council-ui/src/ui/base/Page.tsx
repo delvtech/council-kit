6097fbf50aba9e0269da2caff462a862222ec869
Ryan Goree <goree.ryan@gmail.com>
2023-01-20 00:07:14 -0600

    Use daisy colors to theme skeletons

--- apps/council-ui/src/ui/base/Page.tsx
+++ apps/council-ui/src/ui/base/Page.tsx
@@ -1,9 +1,13 @@
 import { PropsWithChildren, ReactElement } from "react";
+import { SkeletonTheme } from "react-loading-skeleton";
 
 export function Page({ children }: PropsWithChildren): ReactElement {
   return (
-    <div className="flex flex-col max-w-6xl px-4 m-auto mt-16 gap-y-10">
-      {children}
-    </div>
+    // https://daisyui.com/docs/colors
+    <SkeletonTheme baseColor="hsl(var(--b3))" highlightColor="hsl(var(--b1))">
+      <div className="flex flex-col max-w-6xl px-4 m-auto mt-16 gap-y-10 bg-bas">
+        {children}
+      </div>
+    </SkeletonTheme>
   );
 }
