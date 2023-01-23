3b762db023eb894db4efa532b74c2645e4fc28d1
Ryan Goree <goree.ryan@gmail.com>
2023-01-23 17:48:26 -0600

    Remove help cursor from base Tooltip component

--- apps/council-ui/src/ui/base/Tooltip/Tooltip.tsx
+++ apps/council-ui/src/ui/base/Tooltip/Tooltip.tsx
@@ -23,11 +23,7 @@ export function Tooltip({
     <Tooltip2
       {...passThruProps}
       {...propsWithDefaults}
-      className={classNames(
-        Classes.TOOLTIP2_INDICATOR,
-        "cursor-help",
-        className,
-      )}
+      className={classNames(Classes.TOOLTIP2_INDICATOR, className)}
       popoverClassName={classNames(
         styles.popover,
         popoverClassName,
