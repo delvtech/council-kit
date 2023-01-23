89665a67c63db0162141390cd2cc0a945bd77cb5
Danny Delott <dannydelott@gmail.com>
2023-01-23 12:57:14 -0800

    Make vault description link more visible (#149)

--- apps/council-ui/src/ui/vaults/VaultHeader.tsx
+++ apps/council-ui/src/ui/vaults/VaultHeader.tsx
@@ -11,15 +11,17 @@ export function VaultHeader({
   descriptionURL,
   name,
 }: VaultHeaderProps): ReactElement {
+  const vaultName = <h1 className="text-5xl font-bold">{name}</h1>;
+  if (!descriptionURL) {
+    return vaultName;
+  }
+
   return (
-    <div>
-      {descriptionURL ? (
-        <ExternalLink href={descriptionURL} iconSize={30}>
-          <h1 className="inline text-5xl font-bold">{name}</h1>
-        </ExternalLink>
-      ) : (
-        <h1 className="text-5xl font-bold">{name}</h1>
-      )}
+    <div className="flex flex-col gap-1">
+      <h1 className="inline text-5xl font-bold">{name}</h1>
+      <ExternalLink href={descriptionURL} iconSize={18} className="self-start">
+        <span>Learn more about this vault</span>
+      </ExternalLink>
     </div>
   );
 }
