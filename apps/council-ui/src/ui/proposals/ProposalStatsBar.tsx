2b7f463bae125f82f701ab05c8e00d7c230101b1
Cash DeLeon <cashd@users.noreply.github.com>
2023-01-24 16:56:28 -0600

    Update ProposalStatsBar.tsx

--- apps/council-ui/src/ui/proposals/ProposalStatsBar.tsx
+++ apps/council-ui/src/ui/proposals/ProposalStatsBar.tsx
@@ -5,7 +5,6 @@ import { makeVoterURL } from "src/routes";
 import { Address } from "src/ui/base/Address";
 import { formatAddress } from "src/ui/base/formatting/formatAddress";
 import { useDisplayName } from "src/ui/base/formatting/useDisplayName";
-import ExternalLink from "src/ui/base/links/ExternalLink";
 import { Stat } from "src/ui/base/Stat";
 import { ExternalLinkSVG } from "src/ui/base/svg/ExternalLink";
 import { WalletIcon } from "src/ui/base/WalletIcon";
@@ -18,9 +17,6 @@ interface ProposalStatsBarProps {
   endsAtDate: Date | null;
   unlockAtDate: Date | null;
   lastCallAtDate: Date | null;
-
-  forumURL?: string;
-  snapshotURL?: string;
 }
 
 export function ProposalStatsBar({
@@ -31,8 +27,6 @@ export function ProposalStatsBar({
   endsAtDate,
   unlockAtDate,
   lastCallAtDate,
-  forumURL,
-  snapshotURL,
 }: ProposalStatsBarProps): ReactElement {
   const createdByDisplayName = useDisplayName(createdBy);
   return (
@@ -68,20 +62,6 @@ export function ProposalStatsBar({
               <ExternalLinkSVG />
             </Link>
           }
-        />
-      )}
-
-      {forumURL && (
-        <Stat
-          label="Snapshot"
-          value={<ExternalLink href={forumURL}>Results</ExternalLink>}
-        />
-      )}
-
-      {snapshotURL && (
-        <Stat
-          label="Discussion"
-          value={<ExternalLink href={snapshotURL}>Common Wealth</ExternalLink>}
         />
       )}
 
