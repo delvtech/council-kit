d9b0f77dee154fce2fdee9d8891e3d30e5ec0d42
Ryan Goree <goree.ryan@gmail.com>
2023-01-25 13:49:19 -0600

    Add clarifying comments

--- packages/council-sdk/src/models/VotingVault/types.ts
+++ packages/council-sdk/src/models/VotingVault/types.ts
@@ -7,11 +7,13 @@ export interface VoterWithPower {
 
 export interface VoterPowerBreakdown extends VoterWithPower {
   /**
-   * The total voting power from all wallets delegated to this voter.
+   * The total voting power from all wallets delegated to this voter. Does not
+   * include self-delegation.
    */
   votingPowerFromDelegators: string;
   /**
-   * All wallets delegated to this voter with the power they're delegating.
+   * All wallets delegated to this voter with the power they're delegating. Does
+   * not include self-delegation.
    */
   delegators: VoterWithPower[];
 }
