34dc46706263f1a55dc1fa0324864498e9445e84
Ryan Goree <goree.ryan@gmail.com>
2023-01-24 18:19:14 -0600

    Add a margin to the bottom of the page

--- apps/council-ui/src/ui/base/Page.tsx
+++ apps/council-ui/src/ui/base/Page.tsx
@@ -5,7 +5,7 @@ export function Page({ children }: PropsWithChildren): ReactElement {
   return (
     // https://daisyui.com/docs/colors
     <SkeletonTheme baseColor="hsl(var(--b3))" highlightColor="hsl(var(--b1))">
-      <div className="flex flex-col max-w-6xl px-4 m-auto mt-16 gap-y-10 bg-bas">
+      <div className="flex flex-col max-w-6xl px-4 m-auto my-16 gap-y-10 bg-bas">
         {children}
       </div>
     </SkeletonTheme>
