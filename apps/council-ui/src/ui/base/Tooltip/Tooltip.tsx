beed95d229c528f33456eca67db0393352ecb739
Danny Delott <dannydelott@gmail.com>
2023-01-26 12:25:27 -0800

    Update Tooltip and DefinitionTooltip to use react-tooltip implementation

--- apps/council-ui/src/ui/base/Tooltip/Tooltip.tsx
+++ apps/council-ui/src/ui/base/Tooltip/Tooltip.tsx
@@ -1,44 +1,20 @@
-import { Classes, Tooltip2, Tooltip2Props } from "@blueprintjs/popover2";
 import classNames from "classnames";
-import { PropsWithChildren, ReactElement } from "react";
+import { ReactElement } from "react";
+// TODO: Add dependency-cruiser rule to enforce nobody imports directly from
+// react-tooltip outside of this file and app.tsx.
+import { ITooltipWrapper, TooltipWrapper } from "react-tooltip";
 
-// Some elements of the tooltip are unreachable through props and must be styled
-// using external CSS. We use CSS modules to select only this component followed
-// by the `:global` modifier to select the blueprint classes inside of it.
-// see https://github.com/css-modules/css-modules#exceptions
-import styles from "src/ui/base/Tooltip/Tooltip.module.css";
-
-export function Tooltip({
-  className,
-  popoverClassName,
-  placement = "top",
-  interactionKind = "hover",
-  ...passThruProps
-}: PropsWithChildren<Tooltip2Props>): ReactElement {
-  const propsWithDefaults = {
-    placement,
-    interactionKind,
-  };
-  return (
-    <Tooltip2
-      {...passThruProps}
-      {...propsWithDefaults}
-      className={classNames(Classes.TOOLTIP2_INDICATOR, className)}
-      popoverClassName={classNames(
-        styles.popover,
-        popoverClassName,
-        "max-w-xs",
-      )}
-    />
-  );
-}
+// Re-exporting for naming only is usually bad, but in this case it's worth it
+// since TooltipWrapper isn't intuitive to remember and react-tooltip has a
+// `Tooltip` component that we *shouldn't use*.
+export const Tooltip = TooltipWrapper;
 
 export function DefinitionTooltip({
   className,
   ...passThruProps
-}: PropsWithChildren<Tooltip2Props>): ReactElement {
+}: ITooltipWrapper): ReactElement {
   return (
-    <Tooltip
+    <TooltipWrapper
       {...passThruProps}
       className={classNames(
         "cursor-help border-b border-dotted border-current",
