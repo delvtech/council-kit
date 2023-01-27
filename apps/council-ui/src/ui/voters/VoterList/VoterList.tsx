9b5a3c5f74f8f72e79bffe957c106b978778cbcf
Ryan Goree <goree.ryan@gmail.com>
2023-01-27 01:27:24 -0600

    Add sortable table component

--- apps/council-ui/src/ui/voters/VoterList/VoterList.tsx
+++ apps/council-ui/src/ui/voters/VoterList/VoterList.tsx
@@ -5,94 +5,83 @@ import { makeVoterURL } from "src/routes";
 import { formatAddress } from "src/ui/base/formatting/formatAddress";
 import { formatBalance } from "src/ui/base/formatting/formatBalance";
 import {
-  SortDirection,
-  toggleSortDirection,
-} from "src/ui/base/sorting/sortDirection";
-import { SortDirectionStatus } from "src/ui/base/sorting/SortDirectionStatus";
-import { GridTableHeader } from "src/ui/base/tables/GridTableHeader";
-import { GridTableRowLink } from "src/ui/base/tables/GridTableRowLink";
+  SortableGridTable,
+  SortOptions,
+} from "src/ui/base/tables/SortableGridTable";
 import { Tooltip } from "src/ui/base/Tooltip/Tooltip";
 import { WalletIcon } from "src/ui/base/WalletIcon";
 import { VoterRowData } from "src/ui/voters/types";
+
+type SortField = "numberOfDelegators" | "votingPower";
 
 interface VoterListProps {
   voters: VoterRowData[];
   size: number;
   onSizeChange: (newSize: number) => void;
-  defaultSortField?: SortField;
-  defaultSortDirection?: SortDirection;
 }
 
 export function VoterList({
   voters,
   size,
   onSizeChange,
-  defaultSortField,
-  defaultSortDirection,
 }: VoterListProps): ReactElement {
-  const [sortField, setSortField] = useState<SortField | undefined>(
-    defaultSortField,
-  );
-  const [sortDirection, setSortDirection] = useState<SortDirection | undefined>(
-    defaultSortDirection,
-  );
+  const [sortOptions, setSortOptions] = useState<SortOptions<SortField>>({});
 
-  const sortedVoters = useMemo(
-    () =>
-      // don't sort if no sort direction is selected (no default sort).
-      sortDirection ? sortVoters(voters, sortField, sortDirection) : voters,
-    [voters, sortField, sortDirection],
-  );
+  const sortedVoters = useMemo(() => {
+    const { key, direction } = sortOptions;
 
-  function handleSortOptionsChange(field: SortField) {
-    // toggle the direction if the same field is chosen, otherwise reset
-    const startingDirection = field === sortField ? sortDirection : undefined;
-    setSortDirection(toggleSortDirection(startingDirection));
-    setSortField(field);
-  }
+    if (!direction) {
+      return voters;
+    }
+
+    let sorted;
+    switch (key) {
+      case "numberOfDelegators":
+        sorted = voters
+          .slice()
+          .sort((a, b) => a.numberOfDelegators - b.numberOfDelegators);
+        break;
+
+      case "votingPower":
+      default:
+        sorted = voters.slice().sort((a, b) => +a.votingPower - +b.votingPower);
+        break;
+    }
+
+    return direction === "ASC" ? sorted : sorted.reverse();
+  }, [sortOptions, voters]);
 
   return (
     <div className="min-w-[250px]">
-      <GridTableHeader className="grid-cols-[1.5fr_1fr_1fr_56px]">
-        <span>Voter</span>
-        <button
-          className="text-left flex items-center gap-1 hover:bg-base-300"
-          onClick={() => handleSortOptionsChange("numberOfDelegators")}
-        >
-          # of Delegators
-          {sortField === "numberOfDelegators" && (
-            <SortDirectionStatus direction={sortDirection} />
-          )}
-        </button>
-        <button
-          className="text-left flex items-center gap-1 hover:bg-base-300"
-          onClick={() => handleSortOptionsChange("votingPower")}
-        >
-          Voting Power
-          {sortField === "votingPower" && (
-            <SortDirectionStatus direction={sortDirection} />
-          )}
-        </button>
-        <span></span>
-      </GridTableHeader>
-
-      {sortedVoters
-        .slice(0, size)
-        .map(
-          ({
-            address,
-            ensName,
-            votingPower,
-            numberOfDelegators,
-            isGSCMember,
-          }) => {
-            return (
-              <GridTableRowLink
-                key={address}
-                href={makeVoterURL(address)}
-                className="group grid-cols-[1.5fr_1fr_1fr_56px]"
-              >
-                <span className="flex items-center">
+      <SortableGridTable
+        onSort={setSortOptions}
+        colRowClassName="grid-cols-[1.5fr_1fr_1fr_56px]"
+        cols={[
+          "Voter",
+          {
+            name: "# of Delegators",
+            sortKey: "numberOfDelegators",
+          },
+          {
+            name: "Voting Power",
+            sortKey: "votingPower",
+          },
+          "", // extra column for the chevron
+        ]}
+        rowClassName="group grid-cols-[1.5fr_1fr_1fr_56px]"
+        rows={sortedVoters
+          .slice(0, size)
+          .map(
+            ({
+              address,
+              ensName,
+              votingPower,
+              numberOfDelegators,
+              isGSCMember,
+            }) => ({
+              href: makeVoterURL(address),
+              cells: [
+                <span key={address} className="flex items-center">
                   <WalletIcon address={address} className="mr-2" />
                   {ensName ? ensName : formatAddress(address)}
                   {isGSCMember && (
@@ -100,16 +89,16 @@ export function VoterList({
                       <BuildingLibraryIcon className="w-5 h-5 ml-1 fill-warning" />
                     </Tooltip>
                   )}
-                </span>
-                <span>{numberOfDelegators}</span>
-                <span>{formatBalance(votingPower, 0)}</span>
-                <span>
+                </span>,
+                numberOfDelegators,
+                formatBalance(votingPower, 0),
+                <span key={`${address}-chevron`}>
                   <ChevronRightIcon className="w-6 h-6 transition-all stroke-current opacity-40 group-hover:opacity-100" />
-                </span>
-              </GridTableRowLink>
-            );
-          },
-        )}
+                </span>,
+              ],
+            }),
+          )}
+      />
 
       {voters.length > size && (
         <div className="flex flex-col justify-center gap-4 py-8 text-center">
@@ -127,27 +116,4 @@ export function VoterList({
       )}
     </div>
   );
-}
-
-type SortField = "numberOfDelegators" | "votingPower";
-
-function sortVoters(
-  voters: VoterRowData[],
-  field: SortField | undefined,
-  direction: SortDirection = "DESC",
-) {
-  let sorted = voters;
-
-  switch (field) {
-    case "numberOfDelegators":
-      sorted = voters
-        .slice()
-        .sort((a, b) => a.numberOfDelegators - b.numberOfDelegators);
-      break;
-
-    case "votingPower":
-      sorted = voters.slice().sort((a, b) => +a.votingPower - +b.votingPower);
-  }
-
-  return direction === "ASC" ? sorted : sorted.reverse();
 }
