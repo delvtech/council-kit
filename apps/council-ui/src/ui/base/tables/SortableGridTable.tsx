b86f947a45d4428b05b1809660cb800c4a47bc33
Ryan Goree <goree.ryan@gmail.com>
2023-01-27 13:28:50 -0600

    Make voters list sortable (#165)

---
+++ apps/council-ui/src/ui/base/tables/SortableGridTable.tsx
@@ -0,0 +1,151 @@
+import classNames from "classnames";
+import { ReactElement, ReactNode, useState } from "react";
+import { SortDirectionStatus } from "src/ui/base/sorting/SortDirectionStatus";
+import { SortDirection } from "src/ui/base/sorting/types";
+import { GridTableHeader } from "src/ui/base/tables/GridTableHeader";
+import { GridTableRow } from "src/ui/base/tables/GridTableRow";
+import { GridTableRowLink } from "src/ui/base/tables/GridTableRowLink";
+import { UrlObject } from "url";
+
+// Header cells should be uniform, so ReactNodes aren't accepted.
+export type Column<K extends string> =
+  | string
+  | {
+      name: string;
+      className?: string;
+      /**
+       * if defined, the column will be sortable
+       */
+      sortKey?: K;
+    };
+
+export type LinkRow = {
+  cells: ReactNode[];
+  href: string | UrlObject;
+};
+
+export type Row = LinkRow | ReactNode[];
+
+interface SortableGridTableProps<K extends string> {
+  cols: Column<K>[];
+  rows: Row[];
+  onSort?: (sortOptions: SortOptions<K>) => void;
+  headingRowClassName?: string;
+  bodyRowClassName?: string;
+}
+
+/**
+ * A div with `display: grid` pretending to be a table with sortable columns :).
+ * To customize column widths use the `grid-cols-[<col-widths>]` class for
+ * `headingRowClassName` and `bodyRowClassName.
+ * @see https://tailwindcss.com/docs/grid-template-columns#using-custom-values
+ */
+export function SortableGridTable<K extends string>({
+  cols,
+  rows,
+  onSort,
+  headingRowClassName,
+  bodyRowClassName,
+}: SortableGridTableProps<K>): ReactElement {
+  const [sortKey, setSortKey] = useState<K>();
+  const [sortDirection, setSortDirection] = useState<SortDirection>();
+
+  function handleSortChange(key: K) {
+    let newDirection: SortDirection;
+
+    if (key === sortKey) {
+      newDirection = nextSortDirection(sortDirection);
+    } else {
+      newDirection = nextSortDirection();
+      setSortKey(key);
+    }
+    setSortDirection(newDirection);
+
+    onSort?.({
+      key,
+      direction: newDirection,
+    });
+  }
+
+  return (
+    <>
+      <GridTableHeader className={headingRowClassName}>
+        {cols.map((col, i) => {
+          if (typeof col === "string") {
+            return <span key={i}>{col}</span>;
+          }
+
+          if (col.sortKey) {
+            return (
+              <button
+                key={i}
+                className={classNames(
+                  "text-left flex items-center gap-1 hover:bg-base-300",
+                  col.className,
+                )}
+                // safe to cast since the truthiness has already been checked
+                onClick={() => handleSortChange(col.sortKey as K)}
+              >
+                {col.name}
+                {sortKey === col.sortKey && (
+                  <SortDirectionStatus direction={sortDirection} />
+                )}
+              </button>
+            );
+          }
+
+          return (
+            <span key={i} className={col.className}>
+              {col.name}
+            </span>
+          );
+        })}
+      </GridTableHeader>
+
+      {rows.map((row, i) => {
+        if ("href" in row) {
+          return (
+            <GridTableRowLink
+              key={i}
+              href={row.href}
+              className={bodyRowClassName}
+            >
+              {row.cells.map((cell, i) => {
+                if (typeof cell !== "object") {
+                  return <span key={i}>{cell}</span>;
+                }
+                return cell;
+              })}
+            </GridTableRowLink>
+          );
+        }
+        return (
+          <GridTableRow key={i} className={bodyRowClassName}>
+            {row}
+          </GridTableRow>
+        );
+      })}
+    </>
+  );
+}
+
+export interface SortOptions<K extends string> {
+  key?: K;
+  direction?: SortDirection;
+}
+
+// simple state machine for sort state transitions
+function nextSortDirection(
+  currentDirection?: SortDirection,
+  defaultDirection: SortDirection = "DESC",
+): SortDirection {
+  const secondDirection = defaultDirection === "DESC" ? "ASC" : "DESC";
+  switch (currentDirection) {
+    case defaultDirection:
+      return secondDirection;
+    case secondDirection:
+      return undefined;
+    default:
+      return defaultDirection;
+  }
+}
--- apps/council-ui/src/ui/voters/VoterList/VoterList.tsx
+++ apps/council-ui/src/ui/voters/VoterList/VoterList.tsx
@@ -1,14 +1,18 @@
 import { BuildingLibraryIcon } from "@heroicons/react/20/solid";
 import { ChevronRightIcon } from "@heroicons/react/24/outline";
-import { ReactElement } from "react";
+import { ReactElement, useMemo, useState } from "react";
 import { makeVoterURL } from "src/routes";
 import { formatAddress } from "src/ui/base/formatting/formatAddress";
 import { formatBalance } from "src/ui/base/formatting/formatBalance";
-import { GridTableHeader } from "src/ui/base/tables/GridTableHeader";
-import { GridTableRowLink } from "src/ui/base/tables/GridTableRowLink";
+import {
+  SortableGridTable,
+  SortOptions,
+} from "src/ui/base/tables/SortableGridTable";
 import { Tooltip } from "src/ui/base/Tooltip/Tooltip";
 import { WalletIcon } from "src/ui/base/WalletIcon";
 import { VoterRowData } from "src/ui/voters/types";
+
+type SortField = "numberOfDelegators" | "votingPower";
 
 interface VoterListProps {
   voters: VoterRowData[];
@@ -21,49 +25,80 @@ export function VoterList({
   size,
   onSizeChange,
 }: VoterListProps): ReactElement {
+  const [sortOptions, setSortOptions] = useState<SortOptions<SortField>>({});
+
+  const sortedVoters = useMemo(() => {
+    const { key, direction } = sortOptions;
+
+    if (!direction) {
+      return voters;
+    }
+
+    let sorted;
+    switch (key) {
+      case "numberOfDelegators":
+        sorted = voters
+          .slice()
+          .sort((a, b) => a.numberOfDelegators - b.numberOfDelegators);
+        break;
+
+      case "votingPower":
+      default:
+        sorted = voters.slice().sort((a, b) => +a.votingPower - +b.votingPower);
+        break;
+    }
+
+    return direction === "ASC" ? sorted : sorted.reverse();
+  }, [sortOptions, voters]);
+
   return (
     <div className="min-w-[250px]">
-      <GridTableHeader className="grid-cols-[1.5fr_1fr_1fr_56px]">
-        <span>Voter</span>
-        <span># of Delegators</span>
-        <span>Voting Power</span>
-        <span></span>
-      </GridTableHeader>
-
-      {voters
-        .slice(0, size)
-        .map(
-          ({
-            address,
-            ensName,
-            votingPower,
-            numberOfDelegators,
-            isGSCMember,
-          }) => {
-            return (
-              <GridTableRowLink
-                key={address}
-                href={makeVoterURL(address)}
-                className="group grid-cols-[1.5fr_1fr_1fr_56px]"
-              >
-                <span className="flex items-center">
+      <SortableGridTable
+        headingRowClassName="grid-cols-[1.5fr_1fr_1fr_56px]"
+        bodyRowClassName="group grid-cols-[1.5fr_1fr_1fr_56px]"
+        onSort={setSortOptions}
+        cols={[
+          "Voter",
+          {
+            name: "# of Delegators",
+            sortKey: "numberOfDelegators",
+          },
+          {
+            name: "Voting Power",
+            sortKey: "votingPower",
+          },
+          "", // extra column for the chevron
+        ]}
+        rows={sortedVoters
+          .slice(0, size)
+          .map(
+            ({
+              address,
+              ensName,
+              votingPower,
+              numberOfDelegators,
+              isGSCMember,
+            }) => ({
+              href: makeVoterURL(address),
+              cells: [
+                <span key={address} className="flex items-center">
                   <WalletIcon address={address} className="mr-2" />
                   {ensName ? ensName : formatAddress(address)}
                   {isGSCMember && (
                     <Tooltip content="GSC Member">
-                      <BuildingLibraryIcon className="w-5 h-5 ml-1 fill-success" />
+                      <BuildingLibraryIcon className="w-5 h-5 ml-1 fill-warning" />
                     </Tooltip>
                   )}
-                </span>
-                <span>{numberOfDelegators}</span>
-                <span>{formatBalance(votingPower, 0)}</span>
-                <span>
+                </span>,
+                numberOfDelegators,
+                formatBalance(votingPower, 0),
+                <span key={`${address}-chevron`}>
                   <ChevronRightIcon className="w-6 h-6 transition-all stroke-current opacity-40 group-hover:opacity-100" />
-                </span>
-              </GridTableRowLink>
-            );
-          },
-        )}
+                </span>,
+              ],
+            }),
+          )}
+      />
 
       {voters.length > size && (
         <div className="flex flex-col justify-center gap-4 py-8 text-center">
