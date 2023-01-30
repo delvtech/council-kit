e627aff791683c3a6c1cbe63c0c8902f71a5c88e
Cash DeLeon <cashd@users.noreply.github.com>
2023-01-30 16:44:06 -0600

    Create VaultProfileCard.tsx

---
+++ apps/council-ui/src/ui/vaults/VaultProfileCard.tsx
@@ -0,0 +1,63 @@
+import { useRouter } from "next/router";
+import { ReactElement } from "react";
+import { useChainId } from "src/ui/network/useChainId";
+import { LockingVaultProfileCard } from "src/ui/vaults/lockingVault/LockingVaultsProfileCard";
+import { VestingVaultProfileCard } from "src/ui/vaults/vestingVault/VestingVaultProfileCard";
+import { VoterDataByVault } from "src/vaults/getVoterDataByVault";
+import { getAllVaults } from "src/vaults/vaults";
+
+interface VaultProfileCardProps {
+  /** The address of the voting vault */
+  vaultAddress: string;
+  /** Voter data fetched from this specific vault */
+  voterData: VoterDataByVault;
+  /** The address of the voter profile */
+  voterAddress: string;
+}
+
+export function VaultProfileCard({
+  vaultAddress,
+  voterData,
+  voterAddress,
+}: VaultProfileCardProps): ReactElement {
+  const { replace } = useRouter();
+  const chainId = useChainId();
+  const allVaults = getAllVaults(chainId);
+  const vaultConfig = allVaults.find((vault) => vault.address === vaultAddress);
+
+  if (!vaultConfig) {
+    replace("/404");
+  }
+
+  switch (vaultConfig?.type) {
+    case "LockingVault":
+      return (
+        <LockingVaultProfileCard
+          vaultAddress={vaultAddress}
+          userAddress={voterAddress}
+          userVotingPower={voterData.votingPower}
+          userCurrentDelegate={voterData.currentDelegate}
+          userBalance={voterData.balance}
+          userVotersDelegated={voterData.votersDelegated}
+        />
+      );
+
+    case "VestingVault":
+      return (
+        <VestingVaultProfileCard
+          vaultAddress={vaultAddress}
+          userAddress={voterAddress}
+          userVotingPower={voterData.votingPower}
+          userCurrentDelegate={voterData.currentDelegate}
+          userBalance={voterData.balance}
+          userVotersDelegated={voterData.votersDelegated}
+        />
+      );
+
+    case "GSCVault":
+      return <></>;
+
+    default:
+      return <></>;
+  }
+}
