011ca01a1537b054fe58d0fb9604d9bbfcd69262
Danny Delott <dannydelott@gmail.com>
2023-02-01 12:48:47 -0800

    Make useJoinGSC work

--- apps/council-ui/src/ui/vaults/gscVault/useJoinGSC.ts
+++ apps/council-ui/src/ui/vaults/gscVault/useJoinGSC.ts
@@ -1,10 +1,11 @@
-import { GSCVault } from "@council/sdk";
+import { CouncilContext, GSCVault, VotingVault } from "@council/sdk";
 import {
   useMutation,
   UseMutationResult,
   useQueryClient,
 } from "@tanstack/react-query";
 import { Signer } from "ethers";
+import { VaultConfig } from "src/config/CouncilConfig";
 import { makeTransactionErrorToast } from "src/ui/base/toast/makeTransactionErrorToast";
 import { makeTransactionSubmittedToast } from "src/ui/base/toast/makeTransactionSubmittedToast";
 import { makeTransactionSuccessToast } from "src/ui/base/toast/makeTransactionSuccessToast";
@@ -21,15 +22,23 @@ export function useJoinGSC(
   const { context } = useCouncil();
   const queryClient = useQueryClient();
   const chainId = useChainId();
-  const coreVotingVaultAddresses = getCoreVotingVaults(chainId).map(
-    ({ address }) => address,
-  );
   let transactionHash: string;
+  const coreVotingVaultConfigs = getCoreVotingVaults(chainId);
 
   return useMutation({
-    mutationFn: ({ signer }: JoinGSCOptions): Promise<string> => {
+    mutationFn: async ({ signer }: JoinGSCOptions): Promise<string> => {
+      const signerAddress = await signer.getAddress();
+      // collect the vaults that the signer has voting power in. We can only use
+      // those vaults when calling GSCVault.join
+      const vaultsWithVotingPowerForSigner =
+        await getVaultsWithVotingPowerForAccount(
+          signerAddress,
+          coreVotingVaultConfigs,
+          context,
+        );
+
       const gscVault = new GSCVault(gscVaultAddress, context);
-      return gscVault.join(signer, coreVotingVaultAddresses, {
+      return gscVault.join(signer, vaultsWithVotingPowerForSigner, {
         onSubmitted: (hash) => {
           makeTransactionSubmittedToast(
             "Transaction submitted to join the GSC...",
@@ -51,4 +60,25 @@ export function useJoinGSC(
       console.error(error);
     },
   });
+}
+
+// TODO: Use getVaultsWithPower from SDK
+async function getVaultsWithVotingPowerForAccount(
+  account: string,
+  coreVotingVaults: VaultConfig[],
+  context: CouncilContext,
+) {
+  return (
+    await Promise.all(
+      coreVotingVaults.map(async ({ address }) => {
+        const vault = new VotingVault(address, context);
+        return {
+          vaultAddress: vault.address,
+          votingPower: await vault.getVotingPower(account),
+        };
+      }),
+    )
+  )
+    .filter(({ votingPower }) => +votingPower > 0)
+    .map(({ vaultAddress }) => vaultAddress);
 }
