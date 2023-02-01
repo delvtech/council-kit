ec6a4e3637f1fdfca297168f4dece32143219bf7
Ryan Goree <goree.ryan@gmail.com>
2023-01-31 23:45:04 -0600

    Switch proposals list to sortable table

--- apps/council-ui/src/ui/proposals/ProposalsTable.tsx
+++ apps/council-ui/src/ui/proposals/ProposalsTable.tsx
@@ -1,11 +1,11 @@
 import { Ballot } from "@council/sdk";
-import Link from "next/link";
-import { useRouter } from "next/router";
+import { ChevronRightIcon } from "@heroicons/react/20/solid";
 import { ReactElement, useMemo, useState } from "react";
 import { makeProposalURL } from "src/routes";
-import { ChevronRightSVG } from "src/ui/base/svg/ChevronRight";
-import { DownArrowSVG } from "src/ui/base/svg/DownArrow";
-import { UpArrowSVG } from "src/ui/base/svg/UpArrow";
+import {
+  SortableGridTable,
+  SortOptions,
+} from "src/ui/base/tables/SortableGridTable";
 import { ProposalStatus } from "./ProposalStatus";
 
 export interface ProposalRowData {
@@ -25,9 +25,9 @@ interface ProposalsTableProps {
 }
 
 export function ProposalsTable({ rowData }: ProposalsTableProps): ReactElement {
-  const [sortOptions, setSortOptions] = useState<ProposalSortOptions>({
-    field: ProposalSortField.ENDS,
-    direction: ProposalSortDirection.ASC,
+  const [sortOptions, setSortOptions] = useState<SortOptions<SortField>>({
+    direction: "DESC",
+    key: "votingEnds",
   });
 
   const sortedData = useMemo(
@@ -35,180 +35,98 @@ export function ProposalsTable({ rowData }: ProposalsTableProps): ReactElement {
     [sortOptions, rowData],
   );
 
-  const handleSortOptionsChange = (field: ProposalSortField) =>
-    setSortOptions({
-      field,
-      direction: sortToggleStates[sortOptions.direction],
-    });
-
   return (
-    <table className="w-full daisy-table-zebra daisy-table min-w-fit">
-      <thead>
-        <tr>
-          <th>Name</th>
+    <SortableGridTable
+      headingRowClassName="grid-cols-[4fr_1fr_1fr_1fr_56px]"
+      bodyRowClassName="grid-cols-[4fr_1fr_1fr_1fr_56px] items-center"
+      onSort={setSortOptions}
+      cols={[
+        "Name",
+        {
+          cell: "Voting Ends",
+          sortKey: "votingEnds",
+        },
+        {
+          cell: "Quorum",
+          sortKey: "quorum",
+        },
+        "Your Ballot",
+        "", // extra column for the chevron
+      ]}
+      rows={sortedData.map(
+        ({
+          ballot,
+          currentQuorum,
+          id,
+          requiredQuorum,
+          votingContractAddress,
+          votingContractName,
+          votingEnds,
+          sentenceSummary,
+        }) => ({
+          href: makeProposalURL(votingContractAddress, id),
+          cells: [
+            <span key={`${id}-name`}>
+              {votingContractName} Proposal {id}
+              {sentenceSummary && (
+                <p className="opacity-60 text-sm">
+                  {sentenceSummary.length > 80
+                    ? `${sentenceSummary.slice(0, 80)}\u2026` // unicode for horizontal ellipses
+                    : sentenceSummary}
+                </p>
+              )}
+            </span>,
 
-          <th
-            className="cursor-pointer"
-            onClick={() => handleSortOptionsChange(ProposalSortField.ENDS)}
-          >
-            <span className="mr-1 select-none hover:underline">
-              Voting Ends
-            </span>
-            <SortDirectionStatus
-              direction={sortOptions.direction}
-              enabled={sortOptions.field === ProposalSortField.ENDS}
-            />
-          </th>
+            votingEnds?.toLocaleDateString() ?? <em>unknown</em>,
 
-          <th
-            className="cursor-pointer"
-            onClick={() => handleSortOptionsChange(ProposalSortField.QUORUM)}
-          >
-            <span className="mr-1 select-none hover:underline">Quorum</span>
-            <SortDirectionStatus
-              direction={sortOptions.direction}
-              enabled={sortOptions.field === ProposalSortField.QUORUM}
-            />
-          </th>
+            <ProposalStatus
+              key={`${id}-status`}
+              currentQuorum={currentQuorum}
+              requiredQuorum={requiredQuorum}
+              votingEnds={votingEnds}
+              proposalId={id}
+            />,
 
-          <th>Your Ballot</th>
+            ballot ?? <em>Not voted</em>,
 
-          {/* empty header reserved for button */}
-          <th></th>
-        </tr>
-      </thead>
-
-      <tbody>
-        {sortedData.map((proposal) => (
-          <ProposalTableRow
-            {...proposal}
-            key={`${proposal.votingContractName}${proposal.id}`}
-          />
-        ))}
-      </tbody>
-    </table>
+            <ChevronRightIcon
+              key={`${id}-chevron`}
+              className="w-6 h-6 transition-all stroke-current opacity-40 group-hover:opacity-100"
+            />,
+          ],
+        }),
+      )}
+    />
   );
 }
 
-function ProposalTableRow({
-  ballot,
-  currentQuorum,
-  id,
-  requiredQuorum,
-  votingContractAddress,
-  votingContractName,
-  votingEnds,
-  sentenceSummary,
-}: ProposalRowData) {
-  const { push } = useRouter();
-
-  return (
-    <tr
-      onClick={() => push(makeProposalURL(votingContractAddress, id))}
-      className="hover:cursor-pointer"
-    >
-      <td>
-        {votingContractName} Proposal {id}
-        {sentenceSummary && (
-          <p className="opacity-60 text-sm">
-            {sentenceSummary.length > 80
-              ? `${sentenceSummary.slice(0, 80)}\u2026` // unicode for horizontal ellipses
-              : sentenceSummary}
-          </p>
-        )}
-      </td>
-
-      <td>{votingEnds?.toLocaleDateString() ?? <em>unknown</em>}</td>
-
-      <td>
-        <ProposalStatus
-          currentQuorum={currentQuorum}
-          requiredQuorum={requiredQuorum}
-          votingEnds={votingEnds}
-          proposalId={id}
-        />
-      </td>
-
-      <td>{ballot ?? <em>Not voted</em>}</td>
-
-      <td>
-        <button className="daisy-btn daisy-btn-ghost daisy-btn-sm">
-          <Link href={makeProposalURL(votingContractAddress, id)}>
-            <ChevronRightSVG />
-          </Link>
-        </button>
-      </td>
-    </tr>
-  );
-}
-
-interface SortDirectionProps {
-  direction: ProposalSortDirection;
-  enabled: boolean;
-}
-
-function SortDirectionStatus({ direction, enabled }: SortDirectionProps) {
-  if (!enabled) {
-    return null;
-  }
-
-  switch (direction) {
-    case ProposalSortDirection.ASC:
-      return <DownArrowSVG />;
-    case ProposalSortDirection.DESC:
-      return <UpArrowSVG />;
-    default:
-      return null;
-  }
-}
-
-export enum ProposalSortField {
-  ID = "ID",
-  ENDS = "ENDS",
-  QUORUM = "Quorum",
-}
-
-export enum ProposalSortDirection {
-  ASC,
-  DESC,
-}
-
-export interface ProposalSortOptions {
-  field: ProposalSortField;
-  direction: ProposalSortDirection;
-}
-
-// simple state machine for sort state transitions
-const sortToggleStates: Record<ProposalSortDirection, ProposalSortDirection> = {
-  [ProposalSortDirection.ASC]: ProposalSortDirection.DESC,
-  [ProposalSortDirection.DESC]: ProposalSortDirection.ASC,
-};
+type SortField = "votingEnds" | "quorum";
 
 function sortProposalRowData(
-  sort: ProposalSortOptions,
+  { direction = "DESC", key = "votingEnds" }: SortOptions<SortField>,
   data: ProposalRowData[],
 ) {
-  if (sort.field === ProposalSortField.QUORUM) {
-    if (sort.direction === ProposalSortDirection.ASC) {
-      return data.slice().sort((a, b) => +b.currentQuorum - +a.currentQuorum);
-    } else {
+  if (key === "quorum") {
+    if (direction === "ASC") {
       return data.slice().sort((a, b) => +a.currentQuorum - +b.currentQuorum);
+    } else {
+      return data.slice().sort((a, b) => +b.currentQuorum - +a.currentQuorum);
     }
   }
   // safe to assume the desired sort field is voting ends column
   // since there are only two sortable columns.
   else {
-    if (sort.direction === ProposalSortDirection.ASC) {
+    if (direction === "ASC") {
       return data.slice().sort((a, b) => {
         const aTime = a.votingEnds ? a.votingEnds.getTime() : 0;
         const bTime = b.votingEnds ? b.votingEnds.getTime() : 0;
-        return bTime - aTime;
+        return aTime - bTime;
       });
     } else {
       return data.slice().sort((a, b) => {
         const aTime = a.votingEnds ? a.votingEnds.getTime() : 0;
         const bTime = b.votingEnds ? b.votingEnds.getTime() : 0;
-        return aTime - bTime;
+        return bTime - aTime;
       });
     }
   }
