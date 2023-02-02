846d217f27f15e261ac65e92ae2c6a0be0665541
Ryan Goree <goree.ryan@gmail.com>
2023-02-02 01:44:58 -0600

    Update skeleton

--- apps/council-ui/src/ui/voters/skeletons/VoterStatsRowSkeleton.tsx
+++ apps/council-ui/src/ui/voters/skeletons/VoterStatsRowSkeleton.tsx
@@ -5,7 +5,8 @@ import { Stat } from "src/ui/base/Stat";
 export function VoterStatsRowSkeleton(): ReactElement {
   return (
     <div className="flex flex-wrap gap-4">
-      <Stat label="Total Voting Power" value={<Skeleton />} />
+      <Stat label="Voting Power" value={<Skeleton />} />
+      <Stat label="% of TVP" value={<Skeleton />} />
       <Stat label="Proposals voted" value={<Skeleton />} />
       <Stat label="Proposals created" value={<Skeleton />} />
       <Stat label="GSC Member" value={<Skeleton />} />
