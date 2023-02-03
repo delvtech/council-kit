06b44af5e92cf773f9fcfb938734631da9cc72ae
Danny Delott <dannydelott@gmail.com>
2023-02-02 21:01:03 -0800

    Fix tooltips in production (#190)

--- apps/council-ui/next.config.js
+++ apps/council-ui/next.config.js
@@ -15,6 +15,12 @@ module.exports = (phase) => {
   // Production Config
   return {
     reactStrictMode: true,
+
+    // The default Next 13 minifier has a bug which makes react-tooltip not work
+    // in production environments. Turn this off. See:
+    // https://github.com/ReactTooltip/react-tooltip/issues/933
+    swcMinify: false,
+
     basePath: process.env.NEXT_PUBLIC_COUNCIL_UI_BASE_PATH ?? "",
   };
 };
