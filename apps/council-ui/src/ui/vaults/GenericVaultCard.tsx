82b5c8824c2b773e83583a236e411a0c9880352d
Danny Delott <dannydelott@gmail.com>
2023-02-05 20:03:59 -0800

    Refactor GenericVaultCard layout to receipt view

--- apps/council-ui/src/ui/vaults/GenericVaultCard.tsx
+++ apps/council-ui/src/ui/vaults/GenericVaultCard.tsx
@@ -22,32 +22,35 @@ export function GenericVaultCard({
 }: GenericVaultCardProps): ReactElement {
   return (
     <div className="w-full sm:w-80 sm:h-72 daisy-card bg-base-300">
-      <div className="daisy-card-body">
-        <h2 className="text-2xl daisy-card-title">{name}</h2>
-        <Address address={address} className="text-lg" />
-
-        <div className="flex-col daisy-card-actions gap-y-6">
-          <div className="flex flex-row items-start mr-auto text-lg gap-x-6">
-            <div>
-              <h2>
-                <DefinitionTooltip content={TVP_TIP}>TVP</DefinitionTooltip>
-              </h2>
-              <p className="font-bold">{tvp ? formatBalance(tvp) : "None"}</p>
-            </div>
-            <div>
-              <h2>
-                Your{" "}
-                <DefinitionTooltip content={TVP_TIP}>TVP</DefinitionTooltip>
-              </h2>
-              <p className="font-bold">
-                {votingPower ? formatBalance(votingPower) : "None"}
-              </p>
-            </div>
-          </div>
-          <Link href={makeVaultURL(address)}>
-            <button className=" daisy-btn daisy-btn-primary">Open</button>
-          </Link>
+      <div className="daisy-card-body justify-between">
+        <div>
+          <h2 className="text-2xl daisy-card-title">{name}</h2>
+          <Address address={address} className="text-lg" />
         </div>
+
+        <div className="flex-col daisy-card-actions gap-y-2">
+          {/* Total Voting Power */}
+          <div className="flex justify-between w-full">
+            <span>
+              <DefinitionTooltip content={TVP_TIP}>
+                Total voting power:
+              </DefinitionTooltip>
+            </span>
+            <span className="font-bold">
+              {tvp ? formatBalance(tvp) : "None"}
+            </span>
+          </div>
+
+          <div className="flex justify-between w-full">
+            <span>Your voting power:</span>
+            <span className="font-bold">
+              {votingPower ? formatBalance(votingPower) : "None"}
+            </span>
+          </div>
+        </div>
+        <Link href={makeVaultURL(address)}>
+          <button className=" daisy-btn daisy-btn-primary">Open</button>
+        </Link>
       </div>
     </div>
   );
