665b9f0b344c98bb1f0c782e1637b0245757af6f
Danny Delott <dannydelott@gmail.com>
2023-02-06 12:05:13 -0800

    Wire up GSCVaultProfileCard

--- apps/council-ui/src/ui/voters/VoterVaultsList.tsx
+++ apps/council-ui/src/ui/voters/VoterVaultsList.tsx
@@ -1,7 +1,13 @@
+import { useQuery } from "@tanstack/react-query";
 import { getAddress } from "ethers/lib/utils";
 import { ReactElement } from "react";
+import { useCouncil } from "src/ui/council/useCouncil";
+import { useChainId } from "src/ui/network/useChainId";
+import { GSCVaultProfileCard } from "src/ui/vaults/gscVault/GSCVaultProfileCard/GSCVaultProfileCard";
+import { useGSCStatus } from "src/ui/vaults/gscVault/useGSCStatus";
 import { VaultProfileCard } from "src/ui/vaults/VaultProfileCard";
 import { VoterDataByTokenWithDelegationVault } from "src/vaults/getVoterDataByTokenWithDelegationVault";
+import { getGSCCoreVotingVaults } from "src/vaults/vaults";
 import { useEnsName } from "wagmi";
 
 interface VoterVaultsListProps {
@@ -13,13 +19,27 @@ export function VoterVaultsList({
   address,
   vaultsData,
 }: VoterVaultsListProps): ReactElement {
+  const chainId = useChainId();
   const { data: ens } = useEnsName({
     address: getAddress(address as string),
     enabled: !!address,
   });
 
+  const { data: voterVaultsListData } = useVoterVaultsListData(address);
+
+  const { data: gscVaultMembershipStatus } = useGSCStatus(address);
+  const [gscVaultConfig] = getGSCCoreVotingVaults(chainId);
+  const showGSCVaultProfileCard =
+    gscVaultConfig &&
+    gscVaultMembershipStatus &&
+    (gscVaultMembershipStatus === "Member" ||
+      gscVaultMembershipStatus === "Idle") &&
+    voterVaultsListData &&
+    +(voterVaultsListData.qualifyingVotingPowerForGSC || "0");
+
   return (
     <div className="flex flex-wrap w-full gap-6">
+      {/* core voting vaults */}
       {vaultsData.map((voterData) => {
         return (
           <VaultProfileCard
@@ -31,6 +51,28 @@ export function VoterVaultsList({
           />
         );
       })}
+
+      {/* gsc vault */}
+      {showGSCVaultProfileCard ? (
+        <GSCVaultProfileCard
+          voterAddress={address}
+          vaultConfig={gscVaultConfig}
+        />
+      ) : null}
     </div>
   );
+}
+function useVoterVaultsListData(address: string) {
+  const { coreVoting } = useCouncil();
+  return useQuery({
+    queryKey: ["voter-profile-card", address],
+    queryFn: async () => {
+      const qualifyingVotingPowerForGSC = await coreVoting.getVotingPower(
+        address,
+      );
+      return {
+        qualifyingVotingPowerForGSC,
+      };
+    },
+  });
 }
