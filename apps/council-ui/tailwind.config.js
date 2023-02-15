736c8b62a1ef9ed153a9b8331d74808f361da657
Danny Delott <dannydelott@gmail.com>
2023-02-15 15:14:12 -0800

    Update tailwind config

--- apps/council-ui/tailwind.config.js
+++ apps/council-ui/tailwind.config.js
@@ -7,5 +7,22 @@ module.exports = {
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
     prefix: "daisy-",
+    themes: [
+      {
+        elementDaoTheme: {
+          primary: "#005EBE", // principal royal blue
+          secondary: "#6ACDE2", // principal blue
+          accent: "#19C2A3", // topaz
+          neutral: "#242E36", // black
+          "base-100": "#FFFFFF",
+          "base-200": "#F7F8F9",
+          "base-300": "#ECEDF2",
+          info: "#B2CAE3", // blueGrey
+          success: "#007F00", //statusGreen
+          warning: "#FFC300", // goldYellow
+          error: "#F73030", // deepRed
+        },
+      },
+    ],
   },
 };
