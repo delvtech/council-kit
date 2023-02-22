1ed6407506961c0f9ef88aec0feae744d57ccad6
Danny Delott <dannydelott@gmail.com>
2023-02-22 12:17:47 -0600

    Don't support goerli

--- apps/council-ui/src/config/council.config.ts
+++ apps/council-ui/src/config/council.config.ts
@@ -1,12 +1,12 @@
 import { CouncilConfig } from "src/config/CouncilConfig";
-import { goerliCouncilConfig } from "src/config/goerli";
-import { localhostCouncilConfig } from "src/config/localhost";
 import { mainnetCouncilConfig } from "src/config/mainnet";
 
-export type SupportedChainId = 1 | 5 | 31337;
+// Uncomment this to test things on goerli
+// export type SupportedChainId = 1 | 5;
+export type SupportedChainId = 1;
 
 export const councilConfigs: Record<SupportedChainId, CouncilConfig> = {
   1: mainnetCouncilConfig,
-  5: goerliCouncilConfig,
-  31337: localhostCouncilConfig,
+  // Uncomment this to test things on goerli
+  // 5: goerliCouncilConfig,
 };
