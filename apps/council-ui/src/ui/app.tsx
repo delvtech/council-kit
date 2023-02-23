fccdb9822e94b74a3a5c10713da14cb5abf8e4d3
Ryan Goree <goree.ryan@gmail.com>
2023-02-23 10:21:07 -0600

    Add meta tags:

--- apps/council-ui/src/ui/app.tsx
+++ apps/council-ui/src/ui/app.tsx
@@ -1,6 +1,7 @@
 import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { ReactElement } from "react";
 import { Toaster } from "react-hot-toast";
 import { Tooltip, TooltipProvider } from "react-tooltip";
@@ -22,6 +23,49 @@ function App({ Component, pageProps }: AppProps): ReactElement {
           <CouncilClientProvider>
             <TooltipProvider>
               <Toaster />
+              <Head>
+                <link
+                  rel="icon"
+                  type="image/svg+xml"
+                  href="/Element-Dark.png"
+                />
+                <title>Element DAO</title>
+                <meta
+                  name="description"
+                  content="The Element DAO governance portal allows for anyone to view and vote on governance proposals, view voter profiles, and use voting vaults."
+                />
+                <meta
+                  name="viewport"
+                  content="width=device-width, initial-scale=1.0"
+                />
+
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:site" content="@Element_DAO" />
+                <meta name="twitter:title" content="Element DAO" />
+                <meta
+                  name="twitter:description"
+                  content="The Element DAO governance portal allows for anyone to view and vote on governance proposals, view voter profiles, and use voting vaults."
+                />
+                <meta
+                  name="twitter:image"
+                  content="https://i.imgur.com/Re5IWfX.png"
+                />
+
+                <meta property="og:type" content="article" />
+                <meta property="og:title" content="Element DAO" />
+                <meta
+                  property="og:description"
+                  content="The Element DAO governance portal allows for anyone to view and vote on governance proposals, view voter profiles, and use voting vaults."
+                />
+                <meta
+                  property="og:url"
+                  content="https://governance.element.fi"
+                />
+                <meta
+                  property="og:image"
+                  content="https://i.imgur.com/Re5IWfX.png"
+                />
+              </Head>
               <Navigation />
               <main>
                 <Component {...pageProps} />
