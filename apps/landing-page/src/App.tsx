6a46e26b653414513c54edd739c003b845041746
Ryan Goree <goree.ryan@gmail.com>
2023-03-03 01:41:28 -0600

    Add landing page boilerplate

---
+++ apps/landing-page/src/App.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from "react";
+
+function App(): ReactElement {
+  return (
+    <div>
+      <h1 className="text-5xl">Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
