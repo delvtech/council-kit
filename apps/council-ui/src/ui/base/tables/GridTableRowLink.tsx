0cba73053664dcf4570993ae70d35f67f1a89154
Ryan Goree <goree.ryan@gmail.com>
2023-04-18 03:15:37 -0500

    Make zebra striping optional on grid table rows

--- apps/council-ui/src/ui/base/tables/GridTableRowLink.tsx
+++ apps/council-ui/src/ui/base/tables/GridTableRowLink.tsx
@@ -3,6 +3,15 @@ import Link from "next/link";
 import { PropsWithChildren, ReactElement } from "react";
 import { UrlObject } from "url";
 
+export interface GridTableRowLinkProps {
+  href: string | UrlObject;
+  className?: string;
+  /**
+   * Add a bg to odd rows (zebra striping).
+   */
+  zebra?: boolean;
+}
+
 /**
  * A div with `display: grid` pretending to be a table row :).
  * To customize column widths use the `grid-cols-[<col-widths>]` className.
@@ -12,15 +21,17 @@ export function GridTableRowLink({
   href,
   className,
   children,
-}: PropsWithChildren<{
-  href: string | UrlObject;
-  className?: string;
-}>): ReactElement {
+  zebra = true,
+}: PropsWithChildren<GridTableRowLinkProps>): ReactElement {
   return (
     <Link
       href={href}
       className={classNames(
-        "grid grid-flow-col auto-cols-fr odd:bg-base-200 last:rounded-b-lg [&>*]:p-4 [&>*]:overflow-hidden [&>*]:text-ellipsis hover:bg-base-300 transition-all",
+        "grid grid-flow-col auto-cols-fr last:rounded-b-lg [&>*]:p-4 [&>*]:overflow-hidden [&>*]:text-ellipsis hover:bg-base-300 transition-all",
+        {
+          "odd:bg-base-200": zebra,
+          "border-b border-base-200 last:border-none": !zebra,
+        },
         className,
       )}
     >
