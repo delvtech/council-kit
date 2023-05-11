e32e3e1fa78c9a0b716dbc1abf6c0d04847fbb70
Ryan Goree <goree.ryan@gmail.com>
2023-05-11 13:17:12 -0500

    Fix build

--- packages/council-deploy/src/index.ts
+++ packages/council-deploy/src/index.ts
@@ -2,5 +2,3 @@ export * from "src/deployments";
 export * from "src/deployments/types";
 export * from "src/airdrop/accounts";
 export * from "src/airdrop/accounts/types";
-
-export { deployCouncil as deployCouncilExample } from "src/deployCouncil";
