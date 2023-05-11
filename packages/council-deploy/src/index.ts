302bfa7db397cb4c2c4a77eac7466f8c9970565b
Ryan Goree <goree.ryan@gmail.com>
2023-05-11 00:57:13 -0500

    export deploy script from deploy package

--- packages/council-deploy/src/index.ts
+++ packages/council-deploy/src/index.ts
@@ -2,3 +2,5 @@ export * from "src/deployments";
 export * from "src/deployments/types";
 export * from "src/airdrop/accounts";
 export * from "src/airdrop/accounts/types";
+
+export { deployCouncil as deployCouncilExample } from "src/deployCouncil";
