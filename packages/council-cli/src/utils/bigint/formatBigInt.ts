97586ae95dfcf1a9b7622668d289423598d90af6
Ryan Goree <goree.ryan@gmail.com>
2023-05-23 10:57:08 -0500

    Fix formatBigInt util

--- packages/council-cli/src/utils/bigint/formatBigInt.ts
+++ packages/council-cli/src/utils/bigint/formatBigInt.ts
@@ -1,6 +1,6 @@
 export function formatBigInt(bigInt: bigint, decimals = 18): string {
   const bigIntString = bigInt.toString();
-  const whole = bigIntString.slice(0, bigIntString.length - decimals);
-  const part = bigIntString.slice(-decimals).replace(/0*$/, "");
+  const whole = bigIntString.slice(0, -decimals);
+  const part = bigIntString.padStart(decimals, "0").slice(-decimals);
   return `${whole || 0}${part && `.${part}`}`;
 }
