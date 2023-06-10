2b80436a24d835712438b8d41d2b7894ebee37dc
Chakravarthy7102 <medicharlachakravarthi@gmail.com>
2023-06-10 22:39:35 +0530

    prog: add custom hook for tracking the claim progres

--- apps/council-ui/pages/airdrop/index.tsx
+++ apps/council-ui/pages/airdrop/index.tsx
@@ -1,20 +1,54 @@
 import { ReactElement } from "react";
 
 import { BuildingLibraryIcon, WalletIcon } from "@heroicons/react/20/solid";
+import classNames from "classnames";
 import AirdropIcon from "src/ui/airdrop/AirdropIcon";
+import useTrackCurrentAirdropStep from "src/ui/airdrop/hooks/useTrackAirdropSteps";
+
+export enum AIRDROP_STEPS {
+  DEPOSIT_OR_CLAIM = 1,
+  CHOOSE_ACCOUNT = 2,
+  CONFIRM = 3,
+}
+
+export const CURRENT_STEP_KEY = "current_airdrop_step";
 
 export default function AirdropPage(): ReactElement {
+  const { currentStep, completedSteps, updateCurrentStepStatus } =
+    useTrackCurrentAirdropStep();
+
   return (
     <section className="mx-auto max-w-4xl flex flex-col justify-center gap-5 mt-10 text-center">
       <h1 className="text-4xl font-bold">Airdrop Claim</h1>
-      <ul className="daisy-steps w-1/4 m-auto text-sm">
-        <li data-content="1" className="daisy-step daisy-step-neutral">
+      <ul className="daisy-steps md:w-1/2 m-auto text-sm">
+        <li
+          data-content="1"
+          className={classNames("daisy-step", {
+            "daisy-step-neutral": completedSteps.includes(
+              AIRDROP_STEPS.DEPOSIT_OR_CLAIM,
+            ),
+          })}
+        >
           Deposit or Claim
         </li>
-        <li data-content="2" className="daisy-step daisy-step-neutral">
+        <li
+          data-content="2"
+          className={classNames("daisy-step", {
+            "daisy-step-neutral": completedSteps.includes(
+              AIRDROP_STEPS.CHOOSE_ACCOUNT,
+            ),
+          })}
+        >
           Choose account
         </li>
-        <li data-content="3" className="daisy-step daisy-step-neutral">
+        <li
+          data-content="3"
+          className={classNames("daisy-step", {
+            "daisy-step-neutral": completedSteps.includes(
+              AIRDROP_STEPS.CONFIRM,
+            ),
+          })}
+        >
           Confirm
         </li>
       </ul>
@@ -32,7 +66,10 @@ export default function AirdropPage(): ReactElement {
         immediate voting power or <b>claimed</b> directly to a wallet.
       </p>
       <div className="flex justify-center gap-2">
-        <button className="flex text-sm uppercase whitespace-nowrap gap-2 items-center rounded-md px-10 py-2 bg-black text-white">
+        <button
+          onClick={() => updateCurrentStepStatus(AIRDROP_STEPS.CHOOSE_ACCOUNT)}
+          className="flex text-sm uppercase whitespace-nowrap gap-2 items-center rounded-md px-10 py-2 bg-black text-white"
+        >
           <BuildingLibraryIcon className="w-5 h-5 fill-white" />
           Deposit
         </button>
