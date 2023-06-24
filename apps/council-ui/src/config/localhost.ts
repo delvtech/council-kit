b336b52b4bd65fe0a66dff35dc729d19e53e4b22
Ryan Goree <goree.ryan@gmail.com>
2023-06-24 10:35:51 -0500

    Update localhost.ts default config (#394)

--- apps/council-ui/src/config/localhost.ts
+++ apps/council-ui/src/config/localhost.ts
@@ -17,27 +17,6 @@ export const localhostCouncilConfig: CouncilConfig = {
         type: "LockingVault",
         descriptionURL: "https://moreinfo.com",
       },
-      {
-        name: "Vesting Vault",
-        address: "0xA51c1fc2f0D1a1b8494Ed1FE312d7C3a78Ed91C0",
-        type: "VestingVault",
-        descriptionURL: "https://moreinfo.com",
-      },
-    ],
-    proposals: {},
-  },
-
-  gscVoting: {
-    name: "GSC",
-    address: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
-    descriptionURL: "https://moreinfo.com",
-    vaults: [
-      {
-        name: "GSC Vault",
-        address: "0x9A9f2CCfdE556A7E9Ff0848998Aa4a0CFD8863AE",
-        type: "GSCVault",
-        descriptionURL: "https://moreinfo.com",
-      },
     ],
     proposals: {},
   },
