a110c69319bb55f1b1c2c37d6e54a615820c8fd1
Ryan Goree <goree.ryan@gmail.com>
2023-06-24 16:30:41 -0400

    Add set lock duration to sdk

---
+++ apps/council-sdk-starter/src/scripts/setLockDuration.ts
@@ -0,0 +1,28 @@
+import { CouncilContext, VotingContract } from "@council/sdk";
+import { Wallet } from "ethers";
+import { getElementAddress } from "src/addresses/elementAddresses";
+import { provider } from "src/provider";
+
+// wrap the script in an async function so we can await promises
+export async function getProposalResults(): Promise<void> {
+  const addresses = await getElementAddress();
+
+  // create a CouncilContext instance
+  const context = new CouncilContext(provider);
+
+  // Create a VotingContract instance.
+  // The vaults array can be left empty since we won't be fetching any voting
+  // power data.
+  const coreVoting = new VotingContract(addresses.coreVoting, [], context);
+
+  // create a signer for the proposal transaction
+  const signer = new Wallet(process.env.WALLET_PRIVATE_KEY as string, provider);
+
+  const tx = await coreVoting.setLockDuration(signer, 0);
+
+  console.log(`Executed! (${tx})`);
+
+  process.exit();
+}
+
+getProposalResults();
