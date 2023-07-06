50ba8c81aef2a0cf38cdda1d6431be54af6ed514
Ryan Goree <goree.ryan@gmail.com>
2023-07-06 02:27:12 -0500

    Polish claim step

--- apps/council-ui/src/ui/airdrop/Claim.tsx
+++ apps/council-ui/src/ui/airdrop/Claim.tsx
@@ -8,27 +8,36 @@ interface ClaimProps {
 
 export default function Claim({ onBack, onNext }: ClaimProps): ReactElement {
   return (
-    <div className="min-w-[400px] text-start space-y-1">
-      <label className="daisy-label text-lg font-bold" htmlFor="address">
-        Address
-      </label>
-      <input
-        className="w-full daisy-input-bordered daisy-input"
-        type="text"
-        name="address"
-        id="address"
-      />
-      <p className="text-xs my-2">The tokens will be sent to this address.</p>
-      <div className="flex justify-center gap-2 pt-10">
-        <button className="daisy-btn gap-2 px-8" onClick={onBack}>
+    <>
+      <div className="daisy-form-control w-full">
+        <label className="daisy-label text-2xl font-bold" htmlFor="address">
+          Address
+        </label>
+        <input
+          className="w-full daisy-input-bordered daisy-input"
+          type="text"
+          name="address"
+          id="address"
+        />
+        <label className="daisy-label">
+          <span className="label-text-alt">
+            The tokens will be sent to this address.
+          </span>
+        </label>
+      </div>
+      <div className="flex justify-center gap-2">
+        <button className="daisy-btn gap-2 grow" onClick={onBack}>
           <ArrowLeftIcon className="w-4 h-4 fill-current" />
           Back
         </button>
-        <button className="daisy-btn-primary daisy-btn gap-2" onClick={onNext}>
+        <button
+          className="daisy-btn-primary daisy-btn gap-2 grow"
+          onClick={onNext}
+        >
           Continue
           <ArrowRightIcon className="w-4 h-4 fill-current" />
         </button>
       </div>
-    </div>
+    </>
   );
 }
