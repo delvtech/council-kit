80f4e134b3a0790ea59c70d6b4a173928de5acdc
Ryan Goree <goree.ryan@gmail.com>
2023-07-06 02:49:57 -0500

    Polish confirm deposit step

--- apps/council-ui/src/ui/airdrop/ConfirmDeposit.tsx
+++ apps/council-ui/src/ui/airdrop/ConfirmDeposit.tsx
@@ -12,34 +12,30 @@ export default function ConfirmDeposit({
     alert("handle confirm");
   }
   return (
-    <div className="space-y-2">
-      <h3 className="text-lg font-bold">Deposit</h3>
-      <input
-        type="text"
-        value="2,000 MVT"
-        className="daisy-input daisy-input-bordered w-full max-w-xs text-center"
-        disabled
-      />
-      <h3 className="text-lg font-bold">Credit To</h3>
-      <input
-        type="text"
-        value="daryllau.eth"
-        className="daisy-input daisy-input-bordered w-full max-w-xs text-center"
-        disabled
-      />
-      <div className="flex justify-center gap-2 pt-10">
-        <button className="daisy-btn gap-2 px-8" onClick={onBack}>
+    <>
+      <div className="daisy-stats daisy-stats-vertical">
+        <div className="daisy-stat bg-base-200">
+          <div className="daisy-stat-title">Deposit</div>
+          <div className="daisy-stat-value">2,000 MVT</div>
+        </div>
+        <div className="daisy-stat bg-base-200">
+          <div className="daisy-stat-title">Credited To</div>
+          <div className=" daisy-stat-value">daryllau.eth</div>
+        </div>
+      </div>
+      <div className="flex justify-center gap-2">
+        <button className="daisy-btn gap-2 grow" onClick={onBack}>
           <ArrowLeftIcon className="w-4 h-4 fill-current" />
           Back
         </button>
         <button
-          className="daisy-btn daisy-btn-primary gap-2"
+          className="daisy-btn daisy-btn-primary gap-2 grow"
           onClick={handleConfirm}
         >
-          Confirm
           <CheckIcon className="w-4 h-4 fill-current" />
+          Confirm
         </button>
       </div>
-    </div>
+    </>
   );
 }
