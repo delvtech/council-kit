94ca053fb3356314c351d65cdf220c959f1e3730
Ryan Goree <goree.ryan@gmail.com>
2023-07-06 02:46:29 -0500

    Polish deposit step

--- apps/council-ui/src/ui/airdrop/Deposit.tsx
+++ apps/council-ui/src/ui/airdrop/Deposit.tsx
@@ -18,29 +18,36 @@ export default function Deposit({
   }, []);
 
   return (
-    <div className="min-w-[400px] text-start space-y-1">
-      <label className="daisy-label text-lg font-bold" htmlFor="address">
-        Address
-      </label>
-      <input
-        className="w-full daisy-input-bordered daisy-input"
-        type="text"
-        name="address"
-        id="address"
-      />
-      <p className="text-xs">
-        The tokens will be owned by this address in the locking vault.
-      </p>
-      <div className="flex justify-center gap-2 pt-10">
-        <button className="daisy-btn gap-2 px-8" onClick={onBack}>
+    <>
+      <div className="daisy-form-control w-full">
+        <label className="daisy-label text-2xl font-bold" htmlFor="account">
+          Account
+        </label>
+        <input
+          className="w-full daisy-input-bordered daisy-input"
+          type="text"
+          name="account"
+          id="account"
+        />
+        <label className="daisy-label">
+          <span className="label-text-alt">
+            The tokens will be owned by this address in the locking vault.
+          </span>
+        </label>
+      </div>
+      <div className="flex justify-center gap-2">
+        <button className="daisy-btn gap-2 grow" onClick={onBack}>
           <ArrowLeftIcon className="w-4 h-4 fill-current" />
           Back
         </button>
-        <button className="daisy-btn daisy-btn-primary gap-2" onClick={onNext}>
+        <button
+          className="daisy-btn daisy-btn-primary gap-2 grow"
+          onClick={onNext}
+        >
           Continue
           <ArrowRightIcon className="w-4 h-4 fill-current" />
         </button>
       </div>
-    </div>
+    </>
   );
 }
