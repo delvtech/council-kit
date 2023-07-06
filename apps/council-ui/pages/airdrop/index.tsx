5e321f7450a72b9d0671dd035b60b882f20edcaa
Ryan Goree <goree.ryan@gmail.com>
2023-07-06 02:20:40 -0500

    Fix page width and polish initial step

--- apps/council-ui/pages/airdrop/index.tsx
+++ apps/council-ui/pages/airdrop/index.tsx
@@ -18,69 +18,69 @@ export default function AirdropPage(): ReactElement {
   });
 
   return (
-    <section className="mx-auto max-w-5xl flex flex-col justify-center gap-5 mt-10 text-center">
-      <h1 className="text-4xl font-bold">Airdrop Claim</h1>
-      <ul className="daisy-steps md:w-1/2 m-auto text-sm">
-        <li
-          data-content="1"
-          className={classNames("daisy-step", {
-            "daisy-step-primary": getStepNumber(currentStep) >= 1,
-            // "daisy-step-primary": stepsUtils.completedSteps.includes(1),
-          })}
-        >
-          Deposit or Claim
-        </li>
-        <li
-          data-content="2"
-          className={classNames("daisy-step", {
-            "daisy-step-primary": getStepNumber(currentStep) >= 2,
-          })}
-        >
-          Choose account
-        </li>
-        <li
-          data-content="3"
-          className={classNames("daisy-step", {
-            "daisy-step-primary": getStepNumber(currentStep) >= 3,
-          })}
-        >
-          Confirm
-        </li>
-      </ul>
-      <div className="mx-auto flex justify-center flex-col space-y-4">
-        {(() => {
-          switch (currentStep) {
-            case "deposit":
-              return (
-                <Deposit
-                  onBack={() => goToStep("deposit-or-claim")}
-                  onNext={() => goToStep("confirm-deposit")}
-                />
-              );
-            case "confirm-deposit":
-              return <ConfirmDeposit onBack={() => goToStep("deposit")} />;
-
-            case "claim":
-              return (
-                <Claim
-                  onBack={() => goToStep("deposit-or-claim")}
-                  onNext={() => goToStep("confirm-claim")}
-                />
-              );
-            case "confirm-claim":
-              return <ConfirmClaim onBack={() => goToStep("claim")} />;
-
-            case "deposit-or-claim":
-            default:
-              return (
-                <DepositOrClaim
-                  onDeposit={() => goToStep("deposit")}
-                  onClaim={() => goToStep("claim")}
-                />
-              );
-          }
-        })()}
+    <section className="mx-auto max-w-lg flex flex-col justify-center gap-16 mt-14 text-center">
+      <div className="flex flex-col justify-center gap-8">
+        <h1 className="text-5xl font-bold">Airdrop Claim</h1>
+        <ul className="daisy-steps m-auto text-sm w-full">
+          <li
+            data-content="1"
+            className={classNames("daisy-step", {
+              "daisy-step-primary": getStepNumber(currentStep) >= 1,
+              // "daisy-step-primary": stepsUtils.completedSteps.includes(1),
+            })}
+          >
+            Deposit or Claim
+          </li>
+          <li
+            data-content="2"
+            className={classNames("daisy-step", {
+              "daisy-step-primary": getStepNumber(currentStep) >= 2,
+            })}
+          >
+            Choose account
+          </li>
+          <li
+            data-content="3"
+            className={classNames("daisy-step", {
+              "daisy-step-primary": getStepNumber(currentStep) >= 3,
+            })}
+          >
+            Confirm
+          </li>
+        </ul>
       </div>
+      {(() => {
+        switch (currentStep) {
+          case "deposit":
+            return (
+              <Deposit
+                onBack={() => goToStep("deposit-or-claim")}
+                onNext={() => goToStep("confirm-deposit")}
+              />
+            );
+          case "confirm-deposit":
+            return <ConfirmDeposit onBack={() => goToStep("deposit")} />;
+
+          case "claim":
+            return (
+              <Claim
+                onBack={() => goToStep("deposit-or-claim")}
+                onNext={() => goToStep("confirm-claim")}
+              />
+            );
+          case "confirm-claim":
+            return <ConfirmClaim onBack={() => goToStep("claim")} />;
+
+          case "deposit-or-claim":
+          default:
+            return (
+              <DepositOrClaim
+                onDeposit={() => goToStep("deposit")}
+                onClaim={() => goToStep("claim")}
+              />
+            );
+        }
+      })()}
     </section>
   );
 }
--- apps/council-ui/src/ui/airdrop/DepositOrClaim.tsx
+++ apps/council-ui/src/ui/airdrop/DepositOrClaim.tsx
@@ -13,7 +13,7 @@ export default function DepositOrClaim({
 }: DepositOrClaimProps): ReactElement {
   return (
     <>
-      <div className="p-10 mx-auto">
+      <div>
         <p className="mb-2 text-lg">You&apos;ve been airdropped</p>
         <div className="daisy-stats bg-base-200">
           <div className="daisy-stat">
@@ -26,23 +26,25 @@ export default function DepositOrClaim({
           </div>
         </div>
       </div>
-      <p className="mx-auto max-w-lg">
-        These tokens can be <strong>deposited</strong> into the locking vault
-        for immediate voting power or <strong>claimed</strong> directly to a
-        wallet.
-      </p>
-      <div className="flex justify-center gap-2">
-        <button
-          className="daisy-btn-primary daisy-btn gap-2"
-          onClick={onDeposit}
-        >
-          <BuildingLibraryIcon className="w-5 h-5 fill-current" />
-          Deposit
-        </button>
-        <button className="daisy-btn gap-2" onClick={onClaim}>
-          <WalletIcon className="w-5 h-5 fill-current" />
-          Claim
-        </button>
+      <div>
+        <p className="mb-4">
+          These tokens can be <strong>deposited</strong> into the locking vault
+          for immediate voting power or <strong>claimed</strong> directly to a
+          wallet.
+        </p>
+        <div className="flex justify-center gap-2">
+          <button
+            className="daisy-btn-primary daisy-btn gap-2 grow"
+            onClick={onDeposit}
+          >
+            <BuildingLibraryIcon className="w-5 h-5 fill-current" />
+            Deposit
+          </button>
+          <button className="daisy-btn gap-2 grow" onClick={onClaim}>
+            <WalletIcon className="w-5 h-5 fill-current" />
+            Claim
+          </button>
+        </div>
       </div>
     </>
   );
