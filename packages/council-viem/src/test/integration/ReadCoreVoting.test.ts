9c56e2a920694ed82e6460e2e68474d5b92a6cc8
Ryan Goree <goree.ryan@gmail.com>
2024-02-22 01:39:23 -0600

    Disable council-viem integration test and move into src

--- packages/council-viem/src/test/integration/ReadCoreVoting.test.ts
+++ packages/council-viem/src/test/integration/ReadCoreVoting.test.ts
@@ -3,17 +3,14 @@ import { createNetwork, createReadContractFactory } from "src/index";
 import { createPublicClient, http } from "viem";
 import { expect, test } from "vitest";
 
-// const rpcUrl = process.env.TEST_RPC_URL || 'http://localhost:8545';
-const rpcUrl =
-  "https://eth-mainnet.g.alchemy.com/v2/tFR-5zqyPVjqCSE4RJfbHioq5UvbFfV9";
+const rpcUrl = process.env.TEST_RPC_URL || "http://localhost:8545";
 
-test("It fetches events", async () => {
+test.todo("It fetches events", async () => {
   const client = createPublicClient({
     transport: http(rpcUrl),
   });
   const lockingVault = new ReadLockingVault({
-    // address: '0xe7f1725e7734ce288f8367e1bb143e90bb3f0512',
-    address: "0x02Bd4A3b1b95b01F2Aa61655415A5d3EAAcaafdD",
+    address: "0x",
     contractFactory: createReadContractFactory({
       publicClient: client,
     }),
@@ -21,7 +18,7 @@ test("It fetches events", async () => {
   });
 
   const result = await lockingVault.getDelegatorsTo({
-    account: "0x84dC915a87fF4257B553f0AE49ad55cD2167C777",
+    account: "0x",
   });
   console.log("result:", result);
 
