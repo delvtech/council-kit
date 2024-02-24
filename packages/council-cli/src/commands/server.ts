79203e1f1d947d9dac8b885697af7a47397d80bd
Ryan Goree <goree.ryan@gmail.com>
2024-02-23 18:26:07 -0600

    Handle missing hardhat

--- packages/council-cli/src/commands/server.ts
+++ packages/council-cli/src/commands/server.ts
@@ -1,6 +1,16 @@
 import { command } from "clide-js";
-import hre from "hardhat";
+import { execSync } from "node:child_process";
+import signale from "signale";
 import { parseUnits } from "viem";
+
+// This command is only available if the optional hardhat peer dependency is
+// installed so we import it dynamically to avoid crashing if it's not
+// installed.
+let error: Error | undefined;
+const hardhat = await import("hardhat").catch((e) => {
+  error = e;
+  return undefined;
+});
 
 export default command({
   description: "Start a local ethereum node",
@@ -34,6 +44,34 @@ export default command({
   },
 
   handler: async ({ options, end }) => {
+    if (!hardhat) {
+      // https://hardhat.org/hardhat-runner/docs/errors#HH1
+      if (error?.message.startsWith("HH1")) {
+        signale.error(
+          "Must be inside a Hardhat project with a hardhat config file.",
+        );
+      } else if (error?.message.startsWith("HH")) {
+        signale.error(error.message);
+      } else {
+        const globalNodeModules = execSync("npm root -g").toString().trim();
+        const localNodeModules = execSync("npm root").toString().trim();
+        const isGlobal = globalNodeModules === localNodeModules;
+
+        signale.error(error?.message ?? error);
+        signale.error(
+          `This command requires hardhat to be installed. Run ${
+            isGlobal
+              ? "`npm install -g hardhat`"
+              : "`npm install --save-dev hardhat`"
+          } to install hardhat.`,
+        );
+      }
+
+      return end();
+    }
+
+    const hre = hardhat.default;
+
     const host = await options.host();
     const port = await options.port();
     const balance = await options.balance();
