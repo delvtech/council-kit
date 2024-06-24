8bf4a5436761b3f0b8bd627ff1a9a6742c1cca39
Ryan Goree <goree.ryan@gmail.com>
2024-06-24 18:08:50 -0500

    Make wallet connect optional

--- apps/council-ui/src/lib/rainbowKit.ts
+++ apps/council-ui/src/lib/rainbowKit.ts
@@ -1,17 +1,34 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
+import {
+  injectedWallet,
+  metaMaskWallet,
+  rainbowWallet,
+  safeWallet,
+  walletConnectWallet,
+} from "@rainbow-me/rainbowkit/wallets";
 import { chains, transports } from "src/lib/wagmi";
 
-const projectId = process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID;
+const { NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID, NODE_ENV } = process.env;
+const wallets = [injectedWallet, safeWallet, rainbowWallet, metaMaskWallet];
 
-if (!projectId) {
-  throw new Error(
-    "Missing WalletConnect project ID. Please set the NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID variable in your environment.",
+// WalletConnect
+if (NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID) {
+  wallets.push(walletConnectWallet);
+} else if (NODE_ENV === "development") {
+  console.warn(
+    "Missing WalletConnect project ID. Set the NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID variable in your environment to use WalletConnect.",
   );
 }
 
 export const wagmiConfig = getDefaultConfig({
   appName: "Council",
-  projectId,
+  projectId: NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || "0",
   chains: chains as any,
   transports,
+  wallets: [
+    {
+      groupName: "Wallets",
+      wallets,
+    },
+  ],
 });
