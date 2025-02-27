585a2f84e9e6c55d6229f884da328383774321d4
Ryan Goree <goree.ryan@gmail.com>
2025-02-27 13:40:10 -0600

    Update `useCouncilConfig`

--- apps/council-ui/src/ui/config/hooks/useCouncilConfig.ts
+++ apps/council-ui/src/ui/config/hooks/useCouncilConfig.ts
@@ -1,14 +1,12 @@
-import { SupportedChainId, councilConfigs } from "src/config/council.config";
-import { CouncilConfig } from "src/config/types";
+import { SupportedChainId } from "src/config/council.config";
+import { getCouncilConfig } from "src/config/utils/getCouncilConfig";
 import { useSupportedChainId } from "src/ui/network/hooks/useSupportedChainId";
 
-export function useCouncilConfig({
-  chainId,
-}: {
-  chainId?: SupportedChainId;
-} = {}) {
+export function useCouncilConfig<C extends SupportedChainId>(chainId?: C) {
   chainId = useSupportedChainId(chainId);
-  return councilConfigs[chainId] as CouncilConfig & {
-    chainId: SupportedChainId;
-  };
+  const config = getCouncilConfig(chainId);
+  if (!config) {
+    throw new Error(`No config for chain ID ${chainId}`);
+  }
+  return config;
 }
