c191e3c605cc64e154a134a9a9f8335fa0dec932
Ryan Goree <goree.ryan@gmail.com>
2025-02-27 10:32:39 -0600

    Update `tooltipsByStatus`

--- apps/council-ui/src/ui/proposals/tooltips.ts
+++ apps/council-ui/src/ui/proposals/tooltips.ts
@@ -1,19 +1,11 @@
-import { ProposalStatus } from "src/utils/getProposalStatus";
-
-export const EXECUTED_STATUS = "This proposal passed and was executed.";
-export const EXPIRED_STATUS =
-  "This proposal garnered sufficient 'Yes' votes to pass, but was not executed prior to the deadline (i.e., last call).";
-export const FAILED_STATUS =
-  "This proposal did not meet quorum and/or failed to pass.";
-export const IN_PROGRESS_STATUS =
-  "This proposal is currently active and either awaiting more votes or execution.";
-export const UNKNOWN_STATUS =
-  "This proposal is missing information regarding its status.";
+import { ProposalStatus } from "@delvtech/council-js";
 
 export const tooltipByStatus: Record<ProposalStatus, string> = {
-  EXECUTED: EXECUTED_STATUS,
-  EXPIRED: EXPIRED_STATUS,
-  FAILED: FAILED_STATUS,
-  "IN PROGRESS": IN_PROGRESS_STATUS,
-  UNKNOWN: UNKNOWN_STATUS,
+  executed: "This proposal passed and was executed.",
+  expired:
+    "This proposal garnered sufficient 'Yes' votes to pass, but was not executed prior to the deadline (i.e., last call).",
+  failed: "This proposal did not meet quorum and/or failed to pass.",
+  active:
+    "This proposal is currently active and either awaiting more votes or execution.",
+  unknown: "This proposal is missing information regarding its status.",
 };
