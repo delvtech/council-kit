d00c357a3d7bd673f53a88b8fae87c8c6c5ee01f
Ryan Goree <goree.ryan@gmail.com>
2025-02-27 15:50:36 -0600

    Improve `getVotingContractConfig` return type

--- apps/council-ui/src/config/utils/getVotingContractConfig.ts
+++ apps/council-ui/src/config/utils/getVotingContractConfig.ts
@@ -1,11 +1,18 @@
-import { Address } from "@delvtech/drift";
+import { Address, OneOf } from "@delvtech/drift";
 import { SupportedChainId } from "src/config/council.config";
+import {
+  CoreVotingContractConfig,
+  GscVotingContractConfig,
+} from "src/config/types";
 import { getCouncilConfig } from "src/config/utils/getCouncilConfig";
 
-export function getVotingContractConfig(
-  address: Address,
-  chainId: SupportedChainId,
-) {
+export function getVotingContractConfig({
+  address,
+  chainId,
+}: {
+  address: Address;
+  chainId: SupportedChainId;
+}): AnyVotingContractConfig | undefined {
   const { coreVoting, gscVoting } = getCouncilConfig(chainId);
   if (coreVoting.address === address) {
     return { ...coreVoting, isGsc: false };
@@ -14,3 +21,8 @@ export function getVotingContractConfig(
     return { ...gscVoting, isGsc: true };
   }
 }
+
+export type AnyVotingContractConfig = OneOf<
+  | (CoreVotingContractConfig & { isGsc: false })
+  | (GscVotingContractConfig & { isGsc: true })
+>;
