c22d7eaa12bba5ac55b66ac03a2ce8b42e368ecf
Ryan Goree <goree.ryan@gmail.com>
2025-02-27 13:23:55 -0600

    update `useReadCouncil`

--- apps/council-ui/src/ui/sdk/hooks/useReadCouncil.ts
+++ apps/council-ui/src/ui/sdk/hooks/useReadCouncil.ts
@@ -1,9 +1,8 @@
 import { ReadCouncil } from "@delvtech/council-js";
-import { viemAdapter } from "@delvtech/drift-viem";
 import { useMemo } from "react";
 import { SupportedChainId } from "src/config/council.config";
-import { sdkCache } from "src/lib/sdk";
-import { usePublicClient } from "wagmi";
+import { getCouncil } from "src/lib/sdk";
+import { useSupportedChainId } from "src/ui/network/hooks/useSupportedChainId";
 
 /**
  * Use a ReadCouncil instance.
@@ -13,17 +12,6 @@ export function useReadCouncil({
 }: {
   chainId?: SupportedChainId;
 } = {}): ReadCouncil {
-  const publicClient = usePublicClient({ chainId });
-
-  return useMemo(() => {
-    if (!publicClient) {
-      throw new Error("Public client is not available");
-    }
-
-    return new ReadCouncil({
-      adapter: viemAdapter({ publicClient }),
-      cache: sdkCache,
-      chainId,
-    });
-  }, [chainId]);
+  chainId ??= useSupportedChainId(chainId);
+  return useMemo(() => getCouncil({ chainId }), [chainId]);
 }
