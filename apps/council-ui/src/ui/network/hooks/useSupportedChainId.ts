198c1f9b5a652bdd7a4ccc1f26a17fa994a2b182
Ryan Goree <goree.ryan@gmail.com>
2025-02-27 08:25:50 -0600

    Update `useSupportedChainId`

--- apps/council-ui/src/ui/network/hooks/useSupportedChainId.ts
+++ apps/council-ui/src/ui/network/hooks/useSupportedChainId.ts
@@ -1,21 +1,17 @@
-import { useMemo } from "react";
 import { SupportedChainId } from "src/config/council.config";
 import { chains } from "src/lib/wagmi";
 import { useChainId } from "wagmi";
 
 const allChainIds = chains.map(({ id }) => id);
-const defaultChainId = chains[0].id as SupportedChainId;
 
-export function useSupportedChainId(
-  chainId?: SupportedChainId,
-): SupportedChainId {
-  const connectedChainId = useChainId() as SupportedChainId;
-  chainId ??= connectedChainId;
+export function useSupportedChainId<T extends SupportedChainId>(
+  chainId?: T,
+): T {
+  chainId ??= useChainId() as T;
 
-  return useMemo(() => {
-    if (allChainIds.includes(chainId)) {
-      return chainId;
-    }
-    return defaultChainId;
-  }, [chainId]);
+  if (allChainIds.includes(chainId)) {
+    return chainId as T;
+  }
+
+  return chains[0].id as T;
 }
