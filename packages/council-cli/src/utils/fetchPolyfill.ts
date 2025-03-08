39d5a322040550a8bd99306bbc0eff7bafe8ae34
Ryan Goree <goree.ryan@gmail.com>
2025-03-07 22:22:52 -0600

    Update landing page

--- packages/council-cli/src/utils/fetchPolyfill.ts
+++
@@ -1,8 +0,0 @@
-import fetch, { Headers, Request, Response } from "node-fetch-native";
-
-if (!globalThis.fetch) {
-  globalThis.fetch = fetch;
-  globalThis.Headers = Headers;
-  globalThis.Request = Request;
-  globalThis.Response = Response;
-}
